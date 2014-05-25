a97e16cb6a972285679b0178eb16ee068c210d41
Justin CHOI <justin@tosslab.com>
2014-05-26 02:34:09 +0900

    Login, Main Activity UI

--- app/src/main/java/com/tosslab/toss/app/LoginActivity.java
+++ app/src/main/java/com/tosslab/toss/app/LoginActivity.java
@@ -4,16 +4,34 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.Fullscreen;
+import org.androidannotations.annotations.ViewById;
 
-@EActivity(R.layout.activity_main)
-public class MainActivity extends Activity {
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends Activity {
 
+    @ViewById(R.id.edtxt_login_email)
+    EditText edtxtLoginId;
+    @ViewById(R.id.edtxt_login_password)
+    EditText edtxtLoginPassword;
+
+    @Click(R.id.btn_login)
+    void pressLoginButton() {
+        moveToMainActivity();
+    }
+
+    public void moveToMainActivity() {
+        MainActivity_.intent(this).start();
+        finish();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.login, menu);
         return true;
     }
 
