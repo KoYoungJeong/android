b8b87370a02c6233e782a548e9d7eebc30638654
Justin CHOI <justin@tosslab.com>
2014-05-28 04:55:37 +0900

    진행중 : 파일 업로드. 서버 Internal Error 발생.

--- app/src/main/java/com/tosslab/toss/app/MessageListFragment.java
+++ app/src/main/java/com/tosslab/toss/app/MessageListFragment.java
@@ -1,6 +1,12 @@
 package com.tosslab.toss.app;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AbsListView;
@@ -9,6 +15,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.tosslab.toss.app.events.ChooseNaviActionEvent;
+import com.tosslab.toss.app.navigation.MessageItem;
 import com.tosslab.toss.app.navigation.MessageItemListAdapter;
 import com.tosslab.toss.app.network.TossRestClient;
 import com.tosslab.toss.app.network.entities.TossRestPgMessages;
@@ -23,10 +30,25 @@ import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.annotations.FragmentArg;
+import org.androidannotations.annotations.ItemClick;
+import org.androidannotations.annotations.ItemLongClick;
 import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.ViewById;
 import org.androidannotations.annotations.rest.RestService;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.nio.charset.Charset;
 
 import de.greenrobot.event.EventBus;
 
@@ -109,6 +131,15 @@ public class MessageListFragment extends BaseFragment {
     public void onDestroy() {
         EventBus.getDefault().unregister(this);
         super.onPause();
+    }
+
+    /**
+     * Message Item의 Long Click 시, 팝업 메뉴 활성화
+     * @param item
+     */
+    @ItemLongClick
+    void list_messagesItemLongClicked(MessageItem item) {
+        Log.e(TAG, "Long Clicked");
     }
 
     /**
@@ -210,5 +241,68 @@ public class MessageListFragment extends BaseFragment {
             }
             Log.e(TAG, "Send Success");
         }
+    }
+
+    /**
+     * 파일 업로드
+     * TODO : 현재는 Image Upload 만...
+     */
+    @Click(R.id.btn_upload_file)
+    void uploadFile() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Uri targetUri = data.getData();
+            Log.e(TAG, "Get Photo from URI : " + targetUri.toString());
+            String realFilePath = getRealPathFromUri(targetUri);
+            uploadFileInBackground(realFilePath);
+        }
+    }
+
+    @Background
+    void uploadFileInBackground(String fileUri) {
+        // Upload 대상 파일 지정
+        MultiValueMap<String, Object> parts = new LinkedMultiValueMap<String, Object>();
+        parts.add("userFile", new FileSystemResource(new File(fileUri)));
+
+        // Authorization Header 지정
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.set("Authorization", myToken);
+
+        // Create a new RestTemplate instance
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Add the Jackson and String message converters
+        restTemplate.getMessageConverters().add(new MappingJacksonHttpMessageConverter());
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+
+        HttpEntity< MultiValueMap<String, Object>> requestEntity
+                = new HttpEntity<MultiValueMap<String, Object>>(parts, requestHeaders);
+
+        try {
+            String response = restTemplate.postForObject("https://192.168.0.3:3000/inner-api/file",
+                    requestEntity, String.class);
+            Log.d(TAG, response);
+        } catch (RestClientException e) {
+            Log.e(TAG, "Error : " + e);
+        }
+
+
+    }
+
+    // TODO : Poor Implementation
+    private String getRealPathFromUri(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getActivity(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
