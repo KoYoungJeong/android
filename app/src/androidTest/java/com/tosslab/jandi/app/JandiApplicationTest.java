a2d20d0e9fc37fb1f203117900db03f95d538790
jsuch2362 <jsuch2362@gmail.com>
2014-12-02 12:48:40 +0900

    1. Robolectric Test Framework Initialized 2. Check well-done sample test

---
+++ app/src/androidTest/java/com/tosslab/jandi/app/JandiApplicationTest.java
@@ -0,0 +1,25 @@
+package com.tosslab.jandi.app;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+
+/**
+ * Created by jsuch2362 on 14. 12. 1..
+ */
+@Config(manifest = "app/src/main/AndroidManifest.xml",emulateSdk = 18)
+@RunWith(RobolectricGradleTestRunner.class)
+public class JandiApplicationTest {
+
+    @Test
+    public void init() {
+
+        Assert.assertThat(Robolectric.application, is(notNullValue()));
+    }
+}
