ec5ac1dc32cf56a5c9412ad82a19ac2de703d430
Steve <jsuch2362@gmail.com>
2015-01-26 11:45:20 +0900

    improve access by push
        ****************************
    
        1. JND-751 access by push
    
        ****************************
    
        By Steve SeongUg Jung

--- app/src/main/java/com/tosslab/jandi/app/push/PushInterfaceActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/push/PushInterfaceActivity.java
@@ -5,22 +5,18 @@ import android.content.Intent;
 
 import com.tosslab.jandi.app.JandiConstants;
 import com.tosslab.jandi.app.R;
-import com.tosslab.jandi.app.lists.entities.EntityManager;
-import com.tosslab.jandi.app.local.database.account.JandiAccountDatabaseManager;
-import com.tosslab.jandi.app.local.database.entity.JandiEntityDatabaseManager;
-import com.tosslab.jandi.app.network.client.JandiEntityClient;
-import com.tosslab.jandi.app.network.client.JandiEntityClient_;
-import com.tosslab.jandi.app.network.models.ResAccountInfo;
-import com.tosslab.jandi.app.network.models.ResLeftSideMenu;
+import com.tosslab.jandi.app.push.model.JandiInterfaceModel;
 import com.tosslab.jandi.app.ui.intro.IntroActivity_;
+import com.tosslab.jandi.app.ui.maintab.MainTabActivity_;
 import com.tosslab.jandi.app.ui.message.v2.MessageListV2Activity_;
-import com.tosslab.jandi.app.utils.JandiNetworkException;
 
 import org.androidannotations.annotations.AfterInject;
 import org.androidannotations.annotations.Background;
+import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.Extra;
 import org.androidannotations.annotations.UiThread;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Steve SeongUg Jung on 15. 1. 15..
@@ -28,17 +24,17 @@ import org.androidannotations.annotations.UiThread;
 @EActivity(R.layout.activity_intro)
 public class PushInterfaceActivity extends Activity {
 
+    private static final Logger logger = Logger.getLogger(PushInterfaceActivity.class);
     @Extra(JandiConstants.EXTRA_ENTITY_ID)
     int entityId;
-
     @Extra(JandiConstants.EXTRA_ENTITY_TYPE)
     int entityType;
-
     @Extra(JandiConstants.EXTRA_IS_FROM_PUSH)
     boolean isFromPush;
-
     @Extra(JandiConstants.EXTRA_TEAM_ID)
     int teamId;
+    @Bean
+    JandiInterfaceModel jandiInterfaceModel;
 
     @AfterInject
     void initObject() {
@@ -49,57 +45,46 @@ public class PushInterfaceActivity extends Activity {
 
     @Background
     void checkTeamInfo() {
-        ResAccountInfo.UserTeam teamInfo = JandiAccountDatabaseManager.getInstance(PushInterfaceActivity.this).getTeamInfo(teamId);
-        ResAccountInfo.UserTeam selectedTeamInfo = JandiAccountDatabaseManager.getInstance(PushInterfaceActivity.this).getSelectedTeamInfo();
 
-        if (teamInfo != null) {
-            if ((selectedTeamInfo == null || selectedTeamInfo.getTeamId() != teamId)) {
+        if (!jandiInterfaceModel.hasTeamInfo(teamId)) {
+            moveIntroActivity();
+        }
 
-                JandiAccountDatabaseManager.getInstance(PushInterfaceActivity.this).updateSelectedTeam(teamId);
-                JandiEntityClient jandiEntityClient = JandiEntityClient_.getInstance_(PushInterfaceActivity.this);
-
-                try {
-                    ResLeftSideMenu totalEntitiesInfo = jandiEntityClient.getTotalEntitiesInfo();
-                    JandiEntityDatabaseManager.getInstance(PushInterfaceActivity.this).upsertLeftSideMenu(totalEntitiesInfo);
-                    EntityManager.getInstance(PushInterfaceActivity.this).refreshEntity(PushInterfaceActivity.this);
-
-                    moveMessageListActivity();
-
-                } catch (JandiNetworkException e) {
-                    moveIntroActivity();
-                }
-
-            } else {
-                moveMessageListActivity();
-            }
+        if (jandiInterfaceModel.setupSelectedTeam(teamId)) {
+            moveMessageListActivity();
         } else {
             moveIntroActivity();
         }
 
-
     }
+
 
     @UiThread
     void moveIntroActivity() {
         IntroActivity_.intent(PushInterfaceActivity.this)
                 .flags(Intent.FLAG_ACTIVITY_NEW_TASK
-                        | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                        | Intent.FLAG_ACTIVITY_SINGLE_TOP)
+                        | Intent.FLAG_ACTIVITY_CLEAR_TOP)
                 .start();
         finish();
     }
 
     @UiThread
     void moveMessageListActivity() {
-        Intent intent = new Intent(PushInterfaceActivity.this, MessageListV2Activity_.class);
-        intent.putExtra(JandiConstants.EXTRA_ENTITY_ID, entityId);
-        intent.putExtra(JandiConstants.EXTRA_ENTITY_TYPE, entityType);
-        intent.putExtra(JandiConstants.EXTRA_IS_FROM_PUSH, true);
-        intent.putExtra(JandiConstants.EXTRA_TEAM_ID, teamId);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        startActivity(intent);
+
+        if (!jandiInterfaceModel.hasBackStackActivity()) {
+            MainTabActivity_.intent(PushInterfaceActivity.this)
+                    .flags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    .start();
+        }
+
+        MessageListV2Activity_.intent(PushInterfaceActivity.this)
+                .teamId(teamId)
+                .entityId(entityId)
+                .entityType(entityType)
+                .isFromPush(true)
+                .flags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                .start();
+
         finish();
     }
 }
---
+++ app/src/main/java/com/tosslab/jandi/app/push/model/JandiInterfaceModel.java
@@ -0,0 +1,105 @@
+package com.tosslab.jandi.app.push.model;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import com.tosslab.jandi.app.lists.entities.EntityManager;
+import com.tosslab.jandi.app.local.database.account.JandiAccountDatabaseManager;
+import com.tosslab.jandi.app.local.database.entity.JandiEntityDatabaseManager;
+import com.tosslab.jandi.app.network.client.JandiEntityClient;
+import com.tosslab.jandi.app.network.client.JandiEntityClient_;
+import com.tosslab.jandi.app.network.manager.RequestManager;
+import com.tosslab.jandi.app.network.models.ResAccountInfo;
+import com.tosslab.jandi.app.network.models.ResLeftSideMenu;
+import com.tosslab.jandi.app.ui.team.select.model.AccountInfoRequest;
+import com.tosslab.jandi.app.utils.JandiNetworkException;
+
+import org.androidannotations.annotations.EBean;
+import org.androidannotations.annotations.RootContext;
+import org.androidannotations.annotations.SystemService;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+
+/**
+ * Created by Steve SeongUg Jung on 15. 1. 26..
+ */
+@EBean
+public class JandiInterfaceModel {
+
+    private static final Logger logger = Logger.getLogger(JandiInterfaceModel.class);
+
+
+    @RootContext
+    Context context;
+
+    @SystemService
+    ActivityManager activityManager;
+
+    public void refreshAccountInfo() throws JandiNetworkException {
+        AccountInfoRequest accountInfoRequest = AccountInfoRequest.create(context);
+        RequestManager<ResAccountInfo> requestManager = RequestManager.newInstance(context, accountInfoRequest);
+        ResAccountInfo resAccountInfo = requestManager.request();
+
+        JandiAccountDatabaseManager.getInstance(context).upsertAccountInfo(resAccountInfo);
+        JandiAccountDatabaseManager.getInstance(context).upsertAccountEmail(resAccountInfo.getEmails());
+        JandiAccountDatabaseManager.getInstance(context).upsertAccountTeams(resAccountInfo.getMemberships());
+        JandiAccountDatabaseManager.getInstance(context).upsertAccountDevices(resAccountInfo.getDevices());
+
+
+    }
+
+    public boolean hasBackStackActivity() {
+
+        List<ActivityManager.RunningTaskInfo> tasks = activityManager.getRunningTasks(1);
+
+        return tasks != null && tasks.size() > 0 && tasks.get(0).numActivities > 1;
+    }
+
+    public boolean hasTeamInfo(int teamId) {
+        ResAccountInfo.UserTeam teamInfo = JandiAccountDatabaseManager.getInstance(context).getTeamInfo(teamId);
+
+        if (teamInfo == null) {
+
+            try {
+                refreshAccountInfo();
+                teamInfo = JandiAccountDatabaseManager.getInstance(context).getTeamInfo(teamId);
+
+                if (teamInfo == null) {
+                    return false;
+                }
+
+            } catch (JandiNetworkException e) {
+                logger.error("Get Account Info Fail : " + e.getErrorInfo() + " : " + e.httpBody, e);
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    public boolean setupSelectedTeam(int teamId) {
+
+        ResAccountInfo.UserTeam selectedTeamInfo = JandiAccountDatabaseManager.getInstance(context).getSelectedTeamInfo();
+
+        if ((selectedTeamInfo == null || selectedTeamInfo.getTeamId() != teamId)) {
+
+            JandiAccountDatabaseManager.getInstance(context).updateSelectedTeam(teamId);
+            JandiEntityClient jandiEntityClient = JandiEntityClient_.getInstance_(context);
+
+            try {
+                ResLeftSideMenu totalEntitiesInfo = jandiEntityClient.getTotalEntitiesInfo();
+                JandiEntityDatabaseManager.getInstance(context).upsertLeftSideMenu(totalEntitiesInfo);
+                EntityManager.getInstance(context).refreshEntity(totalEntitiesInfo);
+
+                return true;
+            } catch (JandiNetworkException e) {
+                logger.error("Get Entity Info Fail : " + e.getErrorInfo() + " : " + e.httpBody, e);
+                return false;
+            }
+
+        } else {
+            return true;
+        }
+    }
+}
