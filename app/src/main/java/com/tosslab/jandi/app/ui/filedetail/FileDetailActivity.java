d1791910a6dd0501818e55aa27e949c412215dc9
Steve <jsuch2362@gmail.com>
2015-04-20 15:08:35 +0900

    파일 리스트 화면에서 업로드 안되는 현상 수정
        ****************************
    
        1. JND-1397 파일 리스트 화면에서 업로드 안되는 현상 수정
        2. 파일 검색 안되는 경우 l10n 수정
    
        ****************************
    
        By Steve SeongUg Jung

--- app/src/main/java/com/tosslab/jandi/app/ui/filedetail/FileDetailActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/filedetail/FileDetailActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
@@ -189,17 +190,23 @@ public class FileDetailActivity extends BaseAnalyticsActivity {
     }
 
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
-        EventBus.getDefault().register(this);
         trackGaFileDetail(mEntityManager);
     }
 
     @Override
-    public void onPause() {
+    protected void onDestroy() {
+        super.onDestroy();
         EventBus.getDefault().unregister(this);
-        super.onPause();
     }
+
 
     @Override
     protected void onStop() {
