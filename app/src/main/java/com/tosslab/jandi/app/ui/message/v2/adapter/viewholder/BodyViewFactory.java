a7c6f3eecf6504f4311e90c719f96dcf50fa610e
Steve <jsuch2362@gmail.com>
2015-05-06 17:04:37 +0900

    리팩토링
        ****************************
    
        1. JND-1635 리팩토링
    
        ****************************
    
        By Steve SeongUg Jung

--- app/src/main/java/com/tosslab/jandi/app/ui/message/v2/adapter/viewholder/BodyViewFactory.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/message/v2/adapter/viewholder/BodyViewFactory.java
@@ -1,10 +1,16 @@
 package com.tosslab.jandi.app.ui.message.v2.adapter.viewholder;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.tosslab.jandi.app.R;
+import com.tosslab.jandi.app.network.models.ResMessages;
+import com.tosslab.jandi.app.ui.message.to.DummyMessageLink;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Steve SeongUg Jung on 15. 1. 21..
@@ -82,5 +88,82 @@ public class BodyViewFactory {
         }
 
         return null;
+    }
+
+    public static BodyViewHolder.Type getContentType(ResMessages.Link message, ResMessages.Link beforeMessage) {
+
+        if (message instanceof DummyMessageLink) {
+            return BodyViewHolder.Type.Dummy;
+        }
+
+        if (TextUtils.equals(message.status, "event")) {
+            return BodyViewHolder.Type.Event;
+        }
+
+        if (message.message instanceof ResMessages.TextMessage) {
+
+            if (beforeMessage != null
+                    && beforeMessage.message instanceof ResMessages.TextMessage
+                    && message.message.writerId == beforeMessage.message.writerId
+                    && isSince5min(message.message.updateTime, beforeMessage.message.updateTime)
+                    && isSameDay(message, beforeMessage)) {
+                return BodyViewHolder.Type.PureMessage;
+            } else {
+                return BodyViewHolder.Type.Message;
+            }
+
+        } else if (message.message instanceof ResMessages.FileMessage) {
+            String fileType = ((ResMessages.FileMessage) message.message).content.type;
+            if (TextUtils.isEmpty(fileType) || fileType.equals("null")) {
+                return BodyViewHolder.Type.File;
+            }
+            if (fileType.startsWith("image")) {
+                return BodyViewHolder.Type.Image;
+            } else {
+                return BodyViewHolder.Type.File;
+            }
+        } else if (message.message instanceof ResMessages.CommentMessage) {
+
+            if ((beforeMessage == null ||
+                    message.feedbackId == beforeMessage.messageId ||
+                    message.feedbackId == beforeMessage.feedbackId) && isSameDay(message, beforeMessage)) {
+                return BodyViewHolder.Type.PureComment;
+            } else {
+                return BodyViewHolder.Type.FileComment;
+            }
+
+        }
+        return BodyViewHolder.Type.Message;
+    }
+
+    private static boolean isSince5min(Date currentMessageTime, Date beforeMessageTime) {
+
+        long beforeTime = beforeMessageTime.getTime();
+        long currentTime = currentMessageTime.getTime();
+
+        double diffTime = currentTime - beforeTime;
+        if (diffTime / (1000l * 60l * 5) < 1d) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean isSameDay(ResMessages.Link message, ResMessages.Link beforeMessage) {
+
+        if (message == null || beforeMessage == null) {
+            return false;
+        }
+
+        Calendar messageCalendar = Calendar.getInstance();
+        messageCalendar.setTime(message.message.createTime);
+
+        Calendar beforeCalendar = Calendar.getInstance();
+        beforeCalendar.setTime(beforeMessage.message.createTime);
+
+        int messageDay = messageCalendar.get(Calendar.DAY_OF_YEAR);
+        int beforeMessageDay = beforeCalendar.get(Calendar.DAY_OF_YEAR);
+
+        return (messageDay == beforeMessageDay);
     }
 }
