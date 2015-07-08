fd3a896496b037c4e08df95247a33bbc0c7fb278
Steve <jsuch2362@gmail.com>
2015-07-08 11:10:57 +0900

    토픽에 멤버 초대하기 팝업 타이틀 수정
        - JND-2188
    
        [토픽에 멤버 초대하기] 타이틀 수정

--- app/src/main/java/com/tosslab/jandi/app/ui/message/model/menus/InviteCommand.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/message/model/menus/InviteCommand.java
@@ -112,7 +112,7 @@ class InviteCommand implements MenuCommand {
         });
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
-        dialog.setTitle(R.string.title_cdp_invite);
+        dialog.setTitle(R.string.jandi_invite_member_to_topic);
         dialog.setView(view);
         dialog.setPositiveButton(R.string.menu_entity_invite, new DialogInterface.OnClickListener() {
             @Override
