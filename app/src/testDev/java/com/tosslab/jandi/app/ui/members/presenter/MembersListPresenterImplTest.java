7f47ee2f7e3628f02bcff0115894d09f227af021
Steve <jsuch2362@gmail.com>
2015-08-02 13:31:40 +0900

    rxjava learning test 용 코드 작성
       - JND-2430
    
       rxjava learning test 용 코드 작성

---
+++ app/src/testDev/java/com/tosslab/jandi/app/ui/members/presenter/MembersListPresenterImplTest.java
@@ -0,0 +1,67 @@
+package com.tosslab.jandi.app.ui.members.presenter;
+
+import com.jayway.awaitility.Awaitility;
+import com.tosslab.jandi.app.ui.entities.chats.to.ChatChooseItem;
+
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricGradleTestRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.subjects.PublishSubject;
+
+/**
+ * Created by Steve SeongUg Jung on 15. 7. 31..
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+public class MembersListPresenterImplTest {
+
+
+    @Test
+    public void testSearchMember() throws Exception {
+
+        List<ChatChooseItem> chatChooseItems = getMockItems();
+
+        List<ChatChooseItem> testList = new ArrayList<>();
+        PublishSubject<String> publishSubject = PublishSubject.create();
+        publishSubject
+                .throttleWithTimeout(300, TimeUnit.MILLISECONDS)
+                .flatMap(s -> Observable.from(chatChooseItems)
+                        .filter(chatChooseItem -> chatChooseItem.getName().toLowerCase().contains(s.toLowerCase()))
+                        .toSortedList((lhs, rhs) -> lhs.getName().toLowerCase().compareTo(rhs.getName().toLowerCase()))
+                )
+                .subscribe(testList::addAll);
+
+        publishSubject.onNext("he");
+
+        Awaitility.await().until(() -> testList.size() > 0);
+
+        MatcherAssert.assertThat(testList.size(), CoreMatchers.is(IsEqual.equalTo(2)));
+    }
+
+    public List<ChatChooseItem> getMockItems() {
+        List<ChatChooseItem> chatChooseItems = new ArrayList<>();
+
+        ChatChooseItem mock1 = Mockito.mock(ChatChooseItem.class);
+        Mockito.when(mock1.getName()).thenReturn("hahah1");
+
+        ChatChooseItem mock2 = Mockito.mock(ChatChooseItem.class);
+        Mockito.when(mock2.getName()).thenReturn("heheh1");
+        ChatChooseItem mock3 = Mockito.mock(ChatChooseItem.class);
+        Mockito.when(mock3.getName()).thenReturn("hahah2");
+        ChatChooseItem mock4 = Mockito.mock(ChatChooseItem.class);
+        Mockito.when(mock4.getName()).thenReturn("heheh2");
+
+        chatChooseItems.addAll(Arrays.asList(mock1, mock2, mock3, mock4));
+        return chatChooseItems;
+    }
+}
