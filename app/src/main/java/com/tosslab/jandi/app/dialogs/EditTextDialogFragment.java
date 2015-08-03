15c7ff8f5a69b030e9fba34a2d4d9ede44a2d7eb
Steve <jsuch2362@gmail.com>
2015-08-03 16:17:09 +0900

    crashlytics 오류 수정

--- app/src/main/java/com/tosslab/jandi/app/dialogs/EditTextDialogFragment.java
+++ app/src/main/java/com/tosslab/jandi/app/dialogs/EditTextDialogFragment.java
@@ -197,13 +197,13 @@ public class EditTextDialogFragment extends DialogFragment {
         AlertDialog dialog = builder.create();
         dialog.setOnShowListener(dialog1 -> setupPositiveButton(dialog, currentMessage,
                 actionType, currentMessage));
-        setEditTextByPurpose(editTextInput, currentMessage, actionType);
+        setEditTextByPurpose(dialog, editTextInput, currentMessage, actionType);
 
         return dialog;
 
     }
 
-    private void setEditTextByPurpose(final EditText input,
+    private void setEditTextByPurpose(AlertDialog dialog, final EditText input,
                                       final String currentMessage, final int purpose) {
         switch (purpose) {
             case ACTION_MODIFY_PROFILE_PHONE:
