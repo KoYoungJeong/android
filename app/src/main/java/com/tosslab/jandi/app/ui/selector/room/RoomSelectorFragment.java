19b9fd05ef53989d399e1511fba4bd8d8dfa347f
Steve <jsuch2362@gmail.com>
2015-08-17 13:58:00 +0900

    카테고리 선택 UI 기본 만듦

---
+++ app/src/main/java/com/tosslab/jandi/app/ui/selector/room/RoomSelectorFragment.java
@@ -0,0 +1,11 @@
+package com.tosslab.jandi.app.ui.selector.room;
+
+import android.support.v4.app.Fragment;
+
+import com.tosslab.jandi.app.R;
+
+import org.androidannotations.annotations.EFragment;
+
+@EFragment(R.layout.frag_room_selector)
+public class RoomSelectorFragment extends Fragment {
+}
