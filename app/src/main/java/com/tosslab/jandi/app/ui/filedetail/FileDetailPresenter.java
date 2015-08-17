05d15eef9f31d4cfcf6e879bc4386191fa85b5a6
tee <newgto2001@gmail.com>
2015-08-17 12:18:08 +0900

    MentionStar 소스 정리 작업

--- app/src/main/java/com/tosslab/jandi/app/ui/filedetail/FileDetailPresenter.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/filedetail/FileDetailPresenter.java
@@ -480,8 +480,10 @@ public class FileDetailPresenter {
                 activity.getApplicationContext(), fileMessage);
 
         if (mentionControlViewModel == null) {
-            mentionControlViewModel = new MentionControlViewModel(activity,
-                    searchMemberListView, editText, fileCommentListView, sharedTopicIds);
+            mentionControlViewModel = MentionControlViewModel.newInstance(activity,
+                    editText, searchMemberListView, fileCommentListView,
+                    sharedTopicIds,
+                    MentionControlViewModel.MENTION_TYPE_FILE_COMMENT);
             registClipboardListenerforMention();
         }
         mentionControlViewModel.clear();
