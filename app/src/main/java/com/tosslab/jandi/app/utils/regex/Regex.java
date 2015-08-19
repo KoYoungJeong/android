ae727115bb37235896691f106eb1e011f9bb14dc
tee <newgto2001@gmail.com>
2015-08-19 16:03:12 +0900

    [FIX] ip로 된 url을 입력해도 link가 걸릴 수 있도록 개선 (JND-2369) IP형태의  String을 추출할 수 있는 정규 표현식을 사용하여 실제로 IP형태를 블록 처리

--- app/src/main/java/com/tosslab/jandi/app/utils/regex/Regex.java
+++ app/src/main/java/com/tosslab/jandi/app/utils/regex/Regex.java
@@ -10,6 +10,51 @@ import java.util.regex.Pattern;
  */
 public class Regex {
 
+    public static final Pattern VALID_HASHTAG;
+    public static final int VALID_HASHTAG_GROUP_BEFORE = 1;
+    public static final int VALID_HASHTAG_GROUP_HASH = 2;
+    public static final int VALID_HASHTAG_GROUP_TAG = 3;
+    public static final Pattern INVALID_HASHTAG_MATCH_END;
+    public static final Pattern RTL_CHARACTERS;
+    public static final Pattern AT_SIGNS;
+    public static final Pattern VALID_MENTION_OR_LIST;
+    public static final int VALID_MENTION_OR_LIST_GROUP_BEFORE = 1;
+    public static final int VALID_MENTION_OR_LIST_GROUP_AT = 2;
+    public static final int VALID_MENTION_OR_LIST_GROUP_USERNAME = 3;
+    public static final int VALID_MENTION_OR_LIST_GROUP_LIST = 4;
+    public static final Pattern VALID_REPLY;
+    public static final int VALID_REPLY_GROUP_USERNAME = 1;
+    public static final Pattern INVALID_MENTION_MATCH_END;
+    /**
+     * Regex to extract URL (it also includes the text preceding the url).
+     * <p>
+     * This regex does not reflect its name and {@link Regex#VALID_URL_GROUP_URL} match
+     * should be checked in order to match a valid url. This is not ideal, but the behavior is
+     * being kept to ensure backwards compatibility. Ideally this regex should be
+     * implemented with a negative lookbehind as opposed to a negated character class
+     * but lack of JS support increases maint overhead if the logic is different by
+     * platform.
+     */
+
+    public static final Pattern VALID_URL;
+    public static final Pattern VALID_IP_URL;
+    public static final int VALID_URL_GROUP_ALL = 1;
+    public static final int VALID_URL_GROUP_BEFORE = 2;
+    public static final int VALID_URL_GROUP_URL = 3;
+    public static final int VALID_URL_GROUP_PROTOCOL = 4;
+    public static final int VALID_URL_GROUP_DOMAIN = 5;
+    public static final int VALID_URL_GROUP_PORT = 6;
+    public static final int VALID_URL_GROUP_PATH = 7;
+    public static final int VALID_URL_GROUP_QUERY_STRING = 8;
+    public static final Pattern VALID_TCO_URL;
+    public static final Pattern INVALID_URL_WITHOUT_PROTOCOL_MATCH_BEGIN;
+    public static final Pattern VALID_CASHTAG;
+    public static final int VALID_CASHTAG_GROUP_BEFORE = 1;
+
+    /* Begin public constants */
+    public static final int VALID_CASHTAG_GROUP_DOLLAR = 2;
+    public static final int VALID_CASHTAG_GROUP_CASHTAG = 3;
+    public static final Pattern VALID_DOMAIN;
     private static final String URL_VALID_GTLD =
             "(?:(?:" +
                     join(TldLists.GTLDS, "|") +
@@ -18,7 +63,6 @@ public class Regex {
             "(?:(?:" +
                     join(TldLists.CTLDS, "|") +
                     ")(?=[^\\p{Alnum}@]|$))";
-
     private static final String UNICODE_SPACES = "[" +
             "\\u0009-\\u000d" +     //  # White_Space # Cc   [5] <control-0009>..<control-000D>
             "\\u0020" +             // White_Space # Zs       SPACE
@@ -33,7 +77,6 @@ public class Regex {
             "\\u205F" +             // White_Space # Zs       MEDIUM MATHEMATICAL SPACE
             "\\u3000" +             // White_Space # Zs       IDEOGRAPHIC SPACE
             "]";
-
     private static final String LATIN_ACCENTS_CHARS = "\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff" + // Latin-1
             "\\u0100-\\u024f" + // Latin Extended A and B
             "\\u0253\\u0254\\u0256\\u0257\\u0259\\u025b\\u0263\\u0268\\u026f\\u0272\\u0289\\u028b" + // IPA Extensions
@@ -60,10 +103,8 @@ public class Regex {
     private static final String HASHTAG_LETTERS_NUMERALS = HASHTAG_LETTERS + HASHTAG_NUMERALS + HASHTAG_SPECIAL_CHARS;
     private static final String HASHTAG_LETTERS_SET = "[" + HASHTAG_LETTERS + "]";
     private static final String HASHTAG_LETTERS_NUMERALS_SET = "[" + HASHTAG_LETTERS_NUMERALS + "]";
-
     /* URL related hash regex collection */
     private static final String URL_VALID_PRECEEDING_CHARS = "(?:[^A-Z0-9@＠$#＃\u202A-\u202E]|^)";
-
     private static final String URL_VALID_CHARS = "[\\p{Alnum}" + LATIN_ACCENTS_CHARS + "]";
     private static final String URL_VALID_SUBDOMAIN = "(?>(?:" + URL_VALID_CHARS + "[" + URL_VALID_CHARS + "\\-_]*)?" + URL_VALID_CHARS + "\\.)";
     private static final String URL_VALID_DOMAIN_NAME = "(?:(?:" + URL_VALID_CHARS + "[" + URL_VALID_CHARS + "\\-]*)?" + URL_VALID_CHARS + "\\.)";
@@ -71,7 +112,6 @@ public class Regex {
     private static final String URL_VALID_UNICODE_CHARS = "[.[^\\p{Punct}\\s\\p{Z}\\p{InGeneralPunctuation}]]";
     private static final String URL_PUNYCODE = "(?:xn--[0-9a-z]+)";
     private static final String SPECIAL_URL_VALID_CCTLD = "(?:(?:" + "co|tv" + ")(?=[^\\p{Alnum}@]|$))";
-
     private static final String URL_VALID_DOMAIN =
             "(?:" +                                                   // subdomains + domain + TLD
                     URL_VALID_SUBDOMAIN + "+" + URL_VALID_DOMAIN_NAME +   // e.g. www.twitter.com, foo.co.jp, bar.co.uk
@@ -93,14 +133,13 @@ public class Regex {
                     "|(?:" +                                                  // domain + ccTLD + '/'
                     URL_VALID_DOMAIN_NAME + URL_VALID_CCTLD + "(?=/)" +     // e.g. t.co/
                     ")";
-
     private static final String URL_VALID_PORT_NUMBER = "[0-9]++";
-
     private static final String URL_VALID_GENERAL_PATH_CHARS = "[a-z0-9!\\*';:=\\+,.\\$/%#\\[\\]\\-_~\\|&@" + LATIN_ACCENTS_CHARS + "]";
-    /** Allow URL paths to contain up to two nested levels of balanced parens
-     *  1. Used in Wikipedia URLs like /Primer_(film)
-     *  2. Used in IIS sessions like /S(dfd346)/
-     *  3. Used in Rdio URLs like /track/We_Up_(Album_Version_(Edited))/
+    /**
+     * Allow URL paths to contain up to two nested levels of balanced parens
+     * 1. Used in Wikipedia URLs like /Primer_(film)
+     * 2. Used in IIS sessions like /S(dfd346)/
+     * 3. Used in Rdio URLs like /track/We_Up_(Album_Version_(Edited))/
      **/
     private static final String URL_BALANCED_PARENS = "\\(" +
             "(?:" +
@@ -116,12 +155,11 @@ public class Regex {
             ")" +
             ")" +
             "\\)";
-
-    /** Valid end-of-path characters (so /foo. does not gobble the period).
-     *   2. Allow =&# for empty URL parameters and other URL-join artifacts
+    /**
+     * Valid end-of-path characters (so /foo. does not gobble the period).
+     * 2. Allow =&# for empty URL parameters and other URL-join artifacts
      **/
     private static final String URL_VALID_PATH_ENDING_CHARS = "[a-z0-9=_#/\\-\\+" + LATIN_ACCENTS_CHARS + "]|(?:" + URL_BALANCED_PARENS + ")";
-
     private static final String URL_VALID_PATH = "(?:" +
             "(?:" +
             URL_VALID_GENERAL_PATH_CHARS + "*" +
@@ -129,7 +167,6 @@ public class Regex {
             URL_VALID_PATH_ENDING_CHARS +
             ")|(?:@" + URL_VALID_GENERAL_PATH_CHARS + "+/)" +
             ")";
-
     private static final String URL_VALID_URL_QUERY_CHARS = "[a-z0-9!?\\*'\\(\\);:&=\\+\\$/%#\\[\\]\\-_\\.,~\\|@]";
     private static final String URL_VALID_URL_QUERY_ENDING_CHARS = "[a-z0-9_&=#/]";
     private static final String VALID_URL_PATTERN_STRING =
@@ -146,66 +183,13 @@ public class Regex {
                     URL_VALID_URL_QUERY_ENDING_CHARS + ")?" +
                     ")" +
                     ")";
-
     private static final String AT_SIGNS_CHARS = "@\uFF20";
     private static final String DOLLAR_SIGN_CHAR = "\\$";
     private static final String CASHTAG = "[a-z]{1,6}(?:[._][a-z]{1,2})?";
 
-  /* Begin public constants */
-
-    public static final Pattern VALID_HASHTAG;
-    public static final int VALID_HASHTAG_GROUP_BEFORE = 1;
-    public static final int VALID_HASHTAG_GROUP_HASH = 2;
-    public static final int VALID_HASHTAG_GROUP_TAG = 3;
-    public static final Pattern INVALID_HASHTAG_MATCH_END;
-    public static final Pattern RTL_CHARACTERS;
-
-    public static final Pattern AT_SIGNS;
-    public static final Pattern VALID_MENTION_OR_LIST;
-    public static final int VALID_MENTION_OR_LIST_GROUP_BEFORE = 1;
-    public static final int VALID_MENTION_OR_LIST_GROUP_AT = 2;
-    public static final int VALID_MENTION_OR_LIST_GROUP_USERNAME = 3;
-    public static final int VALID_MENTION_OR_LIST_GROUP_LIST = 4;
-
-    public static final Pattern VALID_REPLY;
-    public static final int VALID_REPLY_GROUP_USERNAME = 1;
-
-    public static final Pattern INVALID_MENTION_MATCH_END;
-
-    /**
-     * Regex to extract URL (it also includes the text preceding the url).
-     *
-     * This regex does not reflect its name and {@link Regex#VALID_URL_GROUP_URL} match
-     * should be checked in order to match a valid url. This is not ideal, but the behavior is
-     * being kept to ensure backwards compatibility. Ideally this regex should be
-     * implemented with a negative lookbehind as opposed to a negated character class
-     * but lack of JS support increases maint overhead if the logic is different by
-     * platform.
-     */
-
-    public static final Pattern VALID_URL;
-    public static final int VALID_URL_GROUP_ALL          = 1;
-    public static final int VALID_URL_GROUP_BEFORE       = 2;
-    public static final int VALID_URL_GROUP_URL          = 3;
-    public static final int VALID_URL_GROUP_PROTOCOL     = 4;
-    public static final int VALID_URL_GROUP_DOMAIN       = 5;
-    public static final int VALID_URL_GROUP_PORT         = 6;
-    public static final int VALID_URL_GROUP_PATH         = 7;
-    public static final int VALID_URL_GROUP_QUERY_STRING = 8;
-
-    public static final Pattern VALID_TCO_URL;
-    public static final Pattern INVALID_URL_WITHOUT_PROTOCOL_MATCH_BEGIN;
-
-    public static final Pattern VALID_CASHTAG;
-    public static final int VALID_CASHTAG_GROUP_BEFORE = 1;
-    public static final int VALID_CASHTAG_GROUP_DOLLAR = 2;
-    public static final int VALID_CASHTAG_GROUP_CASHTAG = 3;
-
-    public static final Pattern VALID_DOMAIN;
-
     // initializing in a static synchronized block, there appears to be thread safety issues with Pattern.compile in android
     static {
-        synchronized(Regex.class) {
+        synchronized (Regex.class) {
             VALID_HASHTAG = Pattern.compile("(^|[^&" + HASHTAG_LETTERS_NUMERALS + "])(#|\uFF03)(" + HASHTAG_LETTERS_NUMERALS_SET + "*" + HASHTAG_LETTERS_SET + HASHTAG_LETTERS_NUMERALS_SET + "*)", Pattern.CASE_INSENSITIVE);
             INVALID_HASHTAG_MATCH_END = Pattern.compile("^(?:[#＃]|://)");
             RTL_CHARACTERS = Pattern.compile("[\u0600-\u06FF\u0750-\u077F\u0590-\u05FF\uFE70-\uFEFF]");
@@ -219,6 +203,7 @@ public class Regex {
             VALID_TCO_URL = Pattern.compile("^https?:\\/\\/t\\.co\\/[a-z0-9]+", Pattern.CASE_INSENSITIVE);
             VALID_CASHTAG = Pattern.compile("(^|" + UNICODE_SPACES + ")(" + DOLLAR_SIGN_CHAR + ")(" + CASHTAG + ")" + "(?=$|\\s|\\p{Punct})", Pattern.CASE_INSENSITIVE);
             VALID_DOMAIN = Pattern.compile(URL_VALID_DOMAIN, Pattern.CASE_INSENSITIVE);
+            VALID_IP_URL = Pattern.compile("((?:(http|https|Http|Https|rtsp|Rtsp):\\/\\/(\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b)))");
         }
     }
 
