10396afde67245597fb12273c7d9a0323ea8bbcc
tonyjs <pkojun09@gmail.com>
2015-09-16 22:08:54 +0900

    [fix] A팀 진입중 다른팀에서 메시지를 수신시 App아이콘에 뱃지카운트가 증가합니다. (JND-3283) DB Version 관리 누락 소스 추그

--- app/src/main/java/com/tosslab/jandi/app/local/orm/OrmDatabaseHelper.java
+++ app/src/main/java/com/tosslab/jandi/app/local/orm/OrmDatabaseHelper.java
@@ -125,6 +125,7 @@ public class OrmDatabaseHelper extends OrmLiteSqliteOpenHelper {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+            return;
         }
 
         try {
