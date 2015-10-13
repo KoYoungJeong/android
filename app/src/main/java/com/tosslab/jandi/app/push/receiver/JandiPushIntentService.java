ed2b1755789658b6a47397aeb0f942d36a5fb7e3
Steve <jsuch2362@gmail.com>
2015-10-13 11:36:22 +0900

    [fix] 푸시 queue 처리 개선

--- app/src/main/java/com/tosslab/jandi/app/push/receiver/JandiPushIntentService.java
+++ app/src/main/java/com/tosslab/jandi/app/push/receiver/JandiPushIntentService.java
@@ -6,28 +6,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 
-import com.tosslab.jandi.app.JandiApplication;
 import com.tosslab.jandi.app.events.push.MessagePushEvent;
-import com.tosslab.jandi.app.network.models.ResLeftSideMenu;
 import com.tosslab.jandi.app.push.monitor.PushMonitor;
+import com.tosslab.jandi.app.push.queue.PushHandler;
 import com.tosslab.jandi.app.push.to.PushTO;
 import com.tosslab.jandi.app.utils.AccountUtil;
-import com.tosslab.jandi.app.utils.DateTransformator;
 import com.tosslab.jandi.app.utils.logger.LogUtil;
 
-import java.util.Collections;
-import java.util.concurrent.TimeUnit;
-
 import de.greenrobot.event.EventBus;
-import rx.subjects.PublishSubject;
 
 /**
  * Created by tonyjs on 15. 7. 14..
  */
 public class JandiPushIntentService extends IntentService {
     public static final String TAG = "JANDI.JandiPushIntentService";
-    private PublishSubject<PushTO> pushSubject;
     private JandiPushReceiverModel jandiPushReceiverModel;
+    private PushHandler pushHandler;
 
     public JandiPushIntentService() {
         super(TAG);
@@ -37,26 +31,7 @@ public class JandiPushIntentService extends IntentService {
     public void onCreate() {
         super.onCreate();
         jandiPushReceiverModel = JandiPushReceiverModel_.getInstance_(getApplicationContext());
-        pushSubject = PublishSubject.create();
-        pushSubject
-                .onBackpressureBuffer()
-                .buffer(300, TimeUnit.MILLISECONDS)
-                .filter(pushTOs -> pushTOs != null && !pushTOs.isEmpty())
-                .subscribe(pushTOs -> {
-                    Collections.sort(pushTOs, (lhs, rhs) ->
-                            ((int) (DateTransformator.getTimeFromISO(lhs.getInfo().getCreatedAt())
-                                    - DateTransformator.getTimeFromISO(rhs.getInfo().getCreatedAt()))));
-
-                    PushTO pushTO = pushTOs.get(pushTOs.size() - 1);
-                    notifyPush(JandiApplication.getContext(), pushTO);
-
-                }, Throwable::printStackTrace);
-    }
-
-    @Override
-    public void onDestroy() {
-        pushSubject.onCompleted();
-        super.onDestroy();
+        pushHandler = PushHandler.getInstance();
     }
 
     @Override
@@ -101,55 +76,8 @@ public class JandiPushIntentService extends IntentService {
         // 해당 채팅방에 진입해 있거나 푸시 알림 설정 Off 였을 때
         if (isShowingEntity || !userWantsNotification) {
             postEvent(roomId, pushTOInfo.getRoomType());
-            return;
         }
-
-        pushSubject.onNext(pushTO);
-    }
-
-    private void notifyPush(Context context, PushTO pushTO) {
-        PushTO.PushInfo pushTOInfo;
-        pushTOInfo = pushTO.getInfo();
-        int teamId = pushTOInfo.getTeamId();
-
-        // LeftSideMenu 를 DB를 통해 불러오고 없다면 서버에서 받고 디비에 저장한다.
-        ResLeftSideMenu leftSideMenu = jandiPushReceiverModel.getLeftSideMenuFromDB(teamId);
-        if (leftSideMenu == null) {
-            leftSideMenu = jandiPushReceiverModel.getLeftSideMenuFromServer(teamId);
-            if (leftSideMenu != null) {
-                jandiPushReceiverModel.upsertLeftSideMenu(leftSideMenu);
-            }
-        }
-
-        if (leftSideMenu == null) {
-            showNotification(context, pushTOInfo, false);
-            postEvent(pushTOInfo.getRoomId(), pushTOInfo.getRoomType());
-            return;
-        }
-
-        // 멘션 메시지인 경우 토픽별 푸쉬 on/off 상태는 무시된다.
-        boolean isMentionMessageToMe =
-                jandiPushReceiverModel.isMentionToMe(pushTOInfo.getMentions(), leftSideMenu);
-        if (isMentionMessageToMe) {
-            showNotification(context, pushTOInfo, true);
-        } else {
-            if (PushTO.RoomType.CHAT.getName().equals(pushTOInfo.getRoomType())) {
-                LogUtil.e("tony", "roomType == CHAT");
-                showNotification(context, pushTOInfo, false);
-            } else {
-                boolean isTopicPushOn = jandiPushReceiverModel.isTopicPushOn(leftSideMenu, pushTOInfo.getRoomId());
-                if (isTopicPushOn) {
-                    showNotification(context, pushTOInfo, false);
-                }
-            }
-        }
-
-        postEvent(pushTOInfo.getRoomId(), pushTOInfo.getRoomType());
-    }
-
-    private void showNotification(Context context,
-                                  PushTO.PushInfo pushTOInfo, boolean isMentionMessage) {
-        jandiPushReceiverModel.showNotification(context, pushTOInfo, isMentionMessage);
+        pushHandler.addPushQueue(pushTO);
     }
 
     private void postEvent(int roomId, String roomType) {
@@ -158,5 +86,4 @@ public class JandiPushIntentService extends IntentService {
             eventBus.post(new MessagePushEvent(roomId, roomType));
         }
     }
-
 }
