66d5085dc6c1f1954f27579e1b610fff1889cc67
Steve <jsuch2362@gmail.com>
2015-10-27 16:46:13 +0900

    [fix] 인하우스만 세로 고정 변경

--- app/src/main/java/com/tosslab/jandi/app/utils/activity/ActivityHelper.java
+++ app/src/main/java/com/tosslab/jandi/app/utils/activity/ActivityHelper.java
@@ -2,14 +2,11 @@ package com.tosslab.jandi.app.utils.activity;
 
 import android.app.Activity;
 
-import com.tosslab.jandi.app.ui.settings.model.SettingsModel;
-import com.tosslab.jandi.app.utils.JandiPreference;
-
 public class ActivityHelper {
 
     public static void setOrientation(Activity activity) {
-        String orientation = JandiPreference.getOrientation(activity);
-        int orientationValue = SettingsModel.getOrientationValue(orientation);
-        activity.setRequestedOrientation(orientationValue);
+//        String orientation = JandiPreference.getOrientation(activity);
+//        int orientationValue = SettingsModel.getOrientationValue(orientation);
+//        activity.setRequestedOrientation(orientationValue);
     }
 }
