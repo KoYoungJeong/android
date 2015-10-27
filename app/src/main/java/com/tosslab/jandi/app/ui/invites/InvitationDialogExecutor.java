0b01ae1e8b3940d2fdfb168cfd6dda61ceaf57d7
Steve <jsuch2362@gmail.com>
2015-10-27 18:40:24 +0900

    [fix] 인하우스 오류 수정 * 팝업 디자인 미적용된 3건 처리 * 멤버 리스트 간격 수정

--- app/src/main/java/com/tosslab/jandi/app/ui/invites/InvitationDialogExecutor.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/invites/InvitationDialogExecutor.java
@@ -1,6 +1,6 @@
 package com.tosslab.jandi.app.ui.invites;
 
-import android.app.AlertDialog;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 
