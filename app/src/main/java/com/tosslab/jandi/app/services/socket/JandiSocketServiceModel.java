f846f0ff1891d4bec090a5a517a21fe2ebe946af
tonyjs <pkojun09@gmail.com>
2015-11-06 15:39:30 +0900

    [fix] Socket 연결 전 토큰 확인 로직이 3번의 retry를 가져서 느린 현상 제거

--- app/src/main/java/com/tosslab/jandi/app/services/socket/JandiSocketServiceModel.java
+++ app/src/main/java/com/tosslab/jandi/app/services/socket/JandiSocketServiceModel.java
@@ -30,6 +30,7 @@ import com.tosslab.jandi.app.network.client.EntityClientManager;
 import com.tosslab.jandi.app.network.client.EntityClientManager_;
 import com.tosslab.jandi.app.network.json.JacksonMapper;
 import com.tosslab.jandi.app.network.manager.RequestApiManager;
+import com.tosslab.jandi.app.network.manager.restapiclient.JacksonConvertedSimpleRestApiClient;
 import com.tosslab.jandi.app.network.manager.token.TokenRequestManager;
 import com.tosslab.jandi.app.network.models.ReqAccessToken;
 import com.tosslab.jandi.app.network.models.ResAccessToken;
@@ -446,7 +447,10 @@ public class JandiSocketServiceModel {
     }
 
     public ResAccessToken refreshToken() throws RetrofitError {
-        return TokenRequestManager.getInstance().refreshToken();
+        ResAccessToken accessToken = AccessTokenRepository.getRepository().getAccessToken();
+        String jandiRefreshToken = accessToken.getRefreshToken();
+        ReqAccessToken refreshReqToken = ReqAccessToken.createRefreshReqToken(jandiRefreshToken);
+        return new JacksonConvertedSimpleRestApiClient().getAccessTokenByMainRest(refreshReqToken);
     }
 
     public void createFile(Object object) {
