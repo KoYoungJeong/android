1112ca77b08b0a1095b4da0d81ef9551f17c1698
Steve <jsuch2362@gmail.com>
2015-11-11 14:26:50 +0900

    [fix] message updated list api 변경 ( JND-4082 ) v3 버전업 메세지만 들어오도록 대응

--- app/src/main/java/com/tosslab/jandi/app/network/models/ResUpdateMessages.java
+++ app/src/main/java/com/tosslab/jandi/app/network/models/ResUpdateMessages.java
@@ -1,7 +1,6 @@
 package com.tosslab.jandi.app.network.models;
 
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
-import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 import java.util.List;
@@ -12,100 +11,13 @@ import java.util.List;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class ResUpdateMessages {
-    public int lastLinkId;
-    public UpdateInfo updateInfo;
-    public Alarm alarm;
-    public Event event;
+    public List<ResMessages.Link> messages;
 
     @Override
     public String toString() {
         return "ResUpdateMessages{" +
-                "lastLinkId=" + lastLinkId +
-                ", updateInfo=" + updateInfo +
-                ", alarm=" + alarm +
-                ", event=" + event +
+                "messages=" + messages +
                 '}';
     }
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-    public static class UpdateInfo {
-        public int messageCount;
-
-        public List<ResMessages.Link> messages;
-
-        @Override
-        public String toString() {
-            return "UpdateInfo{" +
-                    "messageCount=" + messageCount +
-                    ", messages=" + messages +
-                    '}';
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-    public static class Alarm {
-        public int alarmCount;
-        public List<AlarmTable> alarmTable;
-
-        @Override
-        public String toString() {
-            return "Alarm{" +
-                    "alarmCount=" + alarmCount +
-                    ", alarmTable=" + alarmTable +
-                    '}';
-        }
-
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-        public static class AlarmTable {
-            public int fromEntity;
-            public List<Integer> toEntity;
-
-            @Override
-            public String toString() {
-                return "AlarmTable{" +
-                        "fromEntity=" + fromEntity +
-                        ", toEntity=" + toEntity +
-                        '}';
-            }
-        }
-    }
-
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-    public static class Event {
-        public int eventCount;
-        public List<EventTable> eventTable;
-
-        @Override
-        public String toString() {
-            return "Event{" +
-                    "eventCount=" + eventCount +
-                    ", eventTable=" + eventTable +
-                    '}';
-        }
-
-        @JsonIgnoreProperties(ignoreUnknown = true)
-        @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
-        public static class EventTable {
-            public int id;
-            public int fromEntity;
-            public List<Integer> toEntity;
-            public String status;
-            public ResMessages.EventInfo eventInfo;
-
-            @Override
-            public String toString() {
-                return "EventTable{" +
-                        "id=" + id +
-                        ", fromEntity=" + fromEntity +
-                        ", toEntity=" + toEntity +
-                        ", status='" + status + '\'' +
-                        ", info=" + eventInfo +
-                        '}';
-            }
-        }
-    }
 }
