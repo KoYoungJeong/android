232740726edc89d31aaea640e1e51a00ad993fda
Steve <jsuch2362@gmail.com>
2015-11-13 19:13:02 +0900

    [fix] AccountHomePresenter 테스트코드 작성 ( JND-4084 )

--- app/src/androidTest/java/com/tosslab/jandi/app/ui/account/AccountHomeActivityTest.java
+++ app/src/androidTest/java/com/tosslab/jandi/app/ui/account/AccountHomeActivityTest.java
@@ -1,10 +1,54 @@
 package com.tosslab.jandi.app.ui.account;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.tosslab.jandi.app.R;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import setup.BaseIniUtil;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.isDialog;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class AccountHomeActivityTest {
 
+    @Rule
+    public ActivityTestRule<AccountHomeActivity_> rule = new ActivityTestRule<>(AccountHomeActivity_.class, true, false);
+    private AccountHomeActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        BaseIniUtil.initData();
+        rule.launchActivity(null);
+
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        activity = rule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        BaseIniUtil.clear();
+
+    }
+
+    @Test
+    public void testOnHelpOptionSelect() throws Exception {
+        activity.onHelpOptionSelect();
+
+        onView(withText(R.string.jandi_account_home_help))
+                .inRoot(isDialog())
+                .check(matches(isDisplayed()));
+    }
 }
--- app/src/androidTest/java/com/tosslab/jandi/app/ui/account/presenter/AccountHomePresenterImplTest.java
+++ app/src/androidTest/java/com/tosslab/jandi/app/ui/account/presenter/AccountHomePresenterImplTest.java
@@ -6,7 +6,11 @@ import android.support.test.runner.AndroidJUnit4;
 
 import com.jayway.awaitility.Awaitility;
 import com.tosslab.jandi.app.JandiApplication;
+import com.tosslab.jandi.app.R;
 import com.tosslab.jandi.app.local.orm.repositories.AccountRepository;
+import com.tosslab.jandi.app.network.manager.RequestApiManager;
+import com.tosslab.jandi.app.network.models.ReqProfileName;
+import com.tosslab.jandi.app.network.models.ResAccountInfo;
 
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +23,7 @@ import setup.BaseIniUtil;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Created by jsuch2362 on 15. 11. 11..
@@ -141,51 +146,105 @@ public class AccountHomePresenterImplTest {
 
     @Test
     public void testOnCreateTeamSelect() throws Exception {
+        accountHomePresenter.onCreateTeamSelect();
 
+        Mockito.verify(viewMock, Mockito.times(1)).loadTeamCreateActivity();
     }
 
     @Test
     public void testOnAccountNameEditClick() throws Exception {
+        String oldName = "click";
+        accountHomePresenter.onAccountNameEditClick(oldName);
+
+        Mockito.verify(viewMock, Mockito.times(1)).showNameEditDialog(Mockito.eq(oldName));
 
     }
 
     @Test
     public void testOnChangeName() throws Exception {
 
+        // Given
+        String originName = AccountRepository.getRepository().getAccountInfo().getName();
+        String newName = "haha";
+
+        final boolean[] finish = {false};
+        Mockito.doAnswer(invocationOnMock -> {
+            finish[0] = true;
+            return invocationOnMock;
+        }).when(viewMock).showSuccessToast(Mockito.anyString());
+
+        // When
+        accountHomePresenter.onChangeName(newName);
+
+        Awaitility.await().until(() -> finish[0]);
+
+        // Then
+        Mockito.verify(viewMock, Mockito.times(1)).showProgressWheel();
+        Mockito.verify(viewMock, Mockito.times(1)).dismissProgressWheel();
+        Mockito.verify(viewMock, Mockito.times(1)).setAccountName(Mockito.eq(newName));
+        Mockito.verify(viewMock, Mockito.times(1)).showSuccessToast(Mockito.eq(JandiApplication.getContext().getString(R.string.jandi_success_update_account_profile)));
+
+        String newSavedName = AccountRepository.getRepository().getAccountInfo().getName();
+        assertThat(newSavedName, is(not(equalTo(originName))));
+        assertThat(newSavedName, is(equalTo(newName)));
+
+        RequestApiManager.getInstance().changeNameByAccountProfileApi(new ReqProfileName(originName));
     }
 
     @Test
     public void testOnTeamCreateAcceptResult() throws Exception {
 
+        final boolean[] finish = {false};
+        Mockito.doAnswer(invocationOnMock -> {
+            finish[0] = true;
+            return invocationOnMock;
+        }).when(viewMock).dismissProgressWheel();
+
+        // When
+        accountHomePresenter.onTeamCreateAcceptResult();
+
+        // Then
+        Awaitility.await().until(() -> finish[0]);
+        Mockito.verify(viewMock, Mockito.times(1)).dismissProgressWheel();
+        Mockito.verify(viewMock, Mockito.times(1)).moveSelectedTeam(Mockito.eq(true));
+
     }
 
     @Test
     public void testOnAccountEmailEditClick() throws Exception {
-
+        accountHomePresenter.onAccountEmailEditClick();
+        Mockito.verify(viewMock, Mockito.times(1)).moveEmailEditClick();
     }
 
     @Test
     public void testOnEmailChooseResult() throws Exception {
+        accountHomePresenter.onEmailChooseResult();
 
-    }
-
-    @Test
-    public void testOnRequestJoin() throws Exception {
-
-    }
-
-    @Test
-    public void testOnRequestIgnore() throws Exception {
+        Mockito.verify(viewMock, Mockito.times(1)).setUserEmailText(Mockito.anyString());
 
     }
 
     @Test
     public void testOnHelpOptionSelect() throws Exception {
+        accountHomePresenter.onHelpOptionSelect();
 
+        Mockito.verify(viewMock, Mockito.times(1)).showHelloDialog();
     }
 
     @Test
     public void testGetTeamInfo() throws Exception {
+
+        final boolean[] finish = {false};
+        Mockito.doAnswer(invocationOnMock -> {
+            finish[0] = true;
+            return invocationOnMock;
+        }).when(viewMock).setTeamInfo(Mockito.any(), Mockito.any());
+
+        accountHomePresenter.getTeamInfo();
+
+        Awaitility.await().until(() -> finish[0]);
+
+        Mockito.verify(viewMock, Mockito.times(1)).setTeamInfo(Mockito.any(), Mockito.any(ResAccountInfo.UserTeam.class));
 
     }
 }
