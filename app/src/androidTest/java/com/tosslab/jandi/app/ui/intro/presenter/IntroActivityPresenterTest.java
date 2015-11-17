ae10d5a60895a610253b524889f3f7ad90fe5a32
tonyjs <pkojun09@gmail.com>
2015-11-17 10:13:34 +0900

    [feature] 테스트 코드 작성(Intro 화면) (JND-4051) mock 을 이용해서 테스트 하도록 변경 IntroActivityModel 안쓰는 로직 삭제, isNetworkConnected 메소드 추가

--- app/src/androidTest/java/com/tosslab/jandi/app/ui/intro/presenter/IntroActivityPresenterTest.java
+++ app/src/androidTest/java/com/tosslab/jandi/app/ui/intro/presenter/IntroActivityPresenterTest.java
@@ -2,7 +2,6 @@ package com.tosslab.jandi.app.ui.intro.presenter;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.wifi.WifiManager;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.ApplicationTestCase;
@@ -10,10 +9,8 @@ import android.test.ApplicationTestCase;
 import com.j256.ormlite.android.apptools.OpenHelperManager;
 import com.tosslab.jandi.app.JandiApplication;
 import com.tosslab.jandi.app.local.orm.OrmDatabaseHelper;
-import com.tosslab.jandi.app.local.orm.repositories.AccessTokenRepository;
-import com.tosslab.jandi.app.local.orm.repositories.AccountRepository;
-import com.tosslab.jandi.app.network.models.ResAccessToken;
-import com.tosslab.jandi.app.network.models.ResAccountInfo;
+import com.tosslab.jandi.app.network.models.ResConfig;
+import com.tosslab.jandi.app.ui.intro.model.IntroActivityModel;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +19,7 @@ import org.junit.runner.RunWith;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by tonyjs on 15. 11. 10..
@@ -38,26 +36,79 @@ public class IntroActivityPresenterTest extends ApplicationTestCase<Application>
         Context context = InstrumentationRegistry.getTargetContext();
 
         OpenHelperManager.getHelper(JandiApplication.getContext(), OrmDatabaseHelper.class).clearAllData();
-        
+
         JandiApplication.setContext(context);
-
-
     }
 
-    //FIXME IntroActivityModel.sleep(Thread.sleep) 으로 인해 타이밍 이슈가 있는 것에 대한 고민 필요
     @Test
     public void testCheckNewVersion() throws Exception {
         Context context = JandiApplication.getContext();
 
-
-        /** 네트워킹이 되지 않는 경우 **/
+        /** 잔디가 점검중인 경우 **/
         // Given
         IntroActivityPresenter.View mockView = mock(IntroActivityPresenter.View.class);
         IntroActivityPresenter presenter = IntroActivityPresenter_.getInstance_(context);
         presenter.setView(mockView);
 
-        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
-        wifiManager.setWifiEnabled(false);
+        ResConfig mockResConfig = mock(ResConfig.class);
+        ResConfig.Maintenance mockMaintenance = mock(ResConfig.Maintenance.class);
+        mockMaintenance.status = true;
+
+        mockResConfig.maintenance = mockMaintenance;
+
+        IntroActivityModel mockModel = mock(IntroActivityModel.class);
+        when(mockModel.isNetworkConnected()).thenReturn(true);
+
+        when(mockModel.getConfigInfo()).thenReturn(mockResConfig);
+
+        presenter.model = mockModel;
+
+        // When
+        presenter.checkNewVersion(context, false);
+
+        // Then
+        verify(mockView, timeout(3000)).showMaintenanceDialog();
+
+        /** 업데이트가 필요한 경우 **/
+        // Given
+        IntroActivityPresenter.View mockView2 = mock(IntroActivityPresenter.View.class);
+        IntroActivityPresenter presenter2 = IntroActivityPresenter_.getInstance_(context);
+        presenter2.setView(mockView2);
+
+        ResConfig mockResConfig2 = mock(ResConfig.class);
+        ResConfig.Versions mockVersions = mock(ResConfig.Versions.class);
+        mockVersions.android = 3;
+        mockResConfig2.versions = mockVersions;
+
+        IntroActivityModel mockModel2 = mock(IntroActivityModel.class);
+        when(mockModel2.isNetworkConnected()).thenReturn(true);
+
+        when(mockModel2.getInstalledAppVersion(context)).thenReturn(2);
+        when(mockModel2.getConfigInfo()).thenReturn(mockResConfig2);
+
+        presenter2.model = mockModel2;
+
+        // When
+        presenter2.checkNewVersion(context, false);
+
+        // Then
+        verify(mockView2, timeout(3000)).showUpdateDialog();
+    }
+
+    /**
+     * checkNewVersion 네트워킹이 되지 않는 경우
+     **/
+    @Test
+    public void testCheckNewVersionWithoutNetworking() throws Exception {
+        Context context = JandiApplication.getContext();
+
+        IntroActivityModel mockModel = mock(IntroActivityModel.class);
+        when(mockModel.isNetworkConnected()).thenReturn(false);
+
+        // Given
+        IntroActivityPresenter.View mockView = mock(IntroActivityPresenter.View.class);
+        IntroActivityPresenter presenter = IntroActivityPresenter_.getInstance_(context);
+        presenter.setView(mockView);
 
         // When
         presenter.checkNewVersion(context, false);
@@ -66,31 +117,28 @@ public class IntroActivityPresenterTest extends ApplicationTestCase<Application>
         verify(mockView, timeout(3000)).moveToIntroTutorialActivity();
 
         /** 네트워킹이 되지 않는 경우, 로그인은 한 경우 **/
-//        // Given
+        // Given
         IntroActivityPresenter.View mockView2 = mock(IntroActivityPresenter.View.class);
         IntroActivityPresenter presenter2 = IntroActivityPresenter_.getInstance_(context);
         presenter2.setView(mockView2);
 
-        ResAccessToken resAccessToken = new ResAccessToken();
-        resAccessToken.setRefreshToken("111dsasd-bsdbsb-2214");
-        AccessTokenRepository.getRepository().upsertAccessToken(resAccessToken);
+        when(mockModel.isNeedLogin()).thenReturn(false);
+        presenter2.model = mockModel;
 
-//        // When
+        // When
         presenter2.checkNewVersion(context, false);
-//
-//        // Then
+
+        // Then
         verify(mockView2, timeout(3000)).showCheckNetworkDialog();
 
         /** 네트워킹이 되지 않는 경우, 로그인은 한 경우, 계정정보가 있는 경우 **/
-//        // Given
+        // Given
         IntroActivityPresenter.View mockView3 = mock(IntroActivityPresenter.View.class);
         IntroActivityPresenter presenter3 = IntroActivityPresenter_.getInstance_(context);
         presenter3.setView(mockView3);
 
-        ResAccountInfo resAccountInfo = new ResAccountInfo();
-        resAccountInfo.setId("13123");
-
-        AccountRepository.getRepository().upsertAccountAllInfo(resAccountInfo);
+        when(mockModel.hasMigration()).thenReturn(true);
+        presenter3.model = mockModel;
 
         // When
         presenter3.checkNewVersion(context, false);
@@ -98,7 +146,20 @@ public class IntroActivityPresenterTest extends ApplicationTestCase<Application>
         // Then
         verify(mockView3, timeout(3000)).moveTeamSelectActivity();
 
+        /** 네트워킹이 되지 않는 경우, 로그인은 한 경우, 계정보가 있는 경우, 선택한 팀이 있는 경우 **/
+        // Given
+        IntroActivityPresenter.View mockView4 = mock(IntroActivityPresenter.View.class);
+        IntroActivityPresenter presenter4 = IntroActivityPresenter_.getInstance_(context);
+        presenter4.setView(mockView4);
 
+        when(mockModel.hasSelectedTeam()).thenReturn(true);
 
+        presenter4.model = mockModel;
+
+        // When
+        presenter4.checkNewVersion(context, false);
+
+        // Then
+        verify(mockView4, timeout(3000)).moveToMainActivity();
     }
 }
