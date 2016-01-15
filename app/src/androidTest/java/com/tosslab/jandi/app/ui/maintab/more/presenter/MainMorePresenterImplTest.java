8cb9dbd29a430a1e77c9bbeea24fc2d3948af2cb
Steve <jsuch2362@gmail.com>
2016-01-15 14:57:05 +0900

    [fix] 테스트 코드 수정

--- app/src/androidTest/java/com/tosslab/jandi/app/ui/maintab/more/presenter/MainMorePresenterImplTest.java
+++ app/src/androidTest/java/com/tosslab/jandi/app/ui/maintab/more/presenter/MainMorePresenterImplTest.java
@@ -14,9 +14,6 @@ import org.mockito.Mockito;
 import setup.BaseInitUtil;
 
 import static com.jayway.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsEqual.equalTo;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyString;
@@ -93,32 +90,13 @@ public class MainMorePresenterImplTest {
 
     @Test
     public void testOnReportUserInfo() throws Exception {
-        VersionClickedInfo versionClickInfo = new VersionClickedInfo();
-
         // when 최초 클릭
         mainMorePresenter.onReportUserInfo();
-        // then 클릭 1회 카운팅
-        assertThat(versionClickInfo.getCount(), is(equalTo(1)));
-
-        // when 두번째 클릭
-        versionClickInfo.setTime(System.currentTimeMillis() - 1);
         mainMorePresenter.onReportUserInfo();
-        // then 클릭 2회 카운팅, 시간은 현재보다 작은 값으로 설정
-        assertThat(versionClickInfo.getCount(), is(equalTo(2)));
-        assertThat(versionClickInfo.getTime(), is(lessThanOrEqualTo(System.currentTimeMillis())));
-
-        // Given 최초 클릭을 현재보다 3초 이전으로 설정
-        versionClickInfo.setTime(System.currentTimeMillis() - 3001);
-        // when
         mainMorePresenter.onReportUserInfo();
-        // then 클릭 정보 초기화
-        assertThat(versionClickInfo.getCount(), is(equalTo(1)));
-
-        // given 3초 이내에 5회 이상 터치 인식
-        versionClickInfo.setTime(System.currentTimeMillis() - 100);
-        versionClickInfo.setCount(5);
-        // when
         mainMorePresenter.onReportUserInfo();
+        mainMorePresenter.onReportUserInfo();
+
         verify(mockView).showBugReportDialog(any());
 
     }
