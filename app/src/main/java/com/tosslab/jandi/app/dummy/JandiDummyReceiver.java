c91ce7463ec7301e42e0d75ad6d420681ca64936
Kim Tae Nyun <newgto2001@gmail.com>
2016-01-18 17:54:15 +0900

    [fix] 삼성 스마트매니저 회피용 로직 적용 (JND-4872) - dummy activity 생성 ( 삼성 스마트 매니져에서 activity실행을 기준으로 정리하는 듯 함) - 기존 task killer 관련 로직도 함께 적용

---
+++ app/src/main/java/com/tosslab/jandi/app/dummy/JandiDummyReceiver.java
@@ -0,0 +1,50 @@
+package com.tosslab.jandi.app.dummy;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.subjects.Subject;
+
+/**
+ * Created by tee on 15. 12. 17..
+ */
+
+// 프로세스가 죽을 경우 푸시를 받기 위한 더미 리시버
+public class JandiDummyReceiver extends BroadcastReceiver {
+    private final static String SMART_MANAGER_PACKAGE_NAME = "com.samsung.android.sm";
+    private boolean isRunnerDummy = false;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())
+                || Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())
+                || Intent.ACTION_POWER_DISCONNECTED.equals(intent.getAction())) {
+            try {
+                context.getPackageManager().getPackageInfo(SMART_MANAGER_PACKAGE_NAME, PackageManager.GET_META_DATA);
+                isRunnerDummy = true;
+            } catch (PackageManager.NameNotFoundException e) {
+                isRunnerDummy = false;
+            }
+        } else if (Intent.ACTION_PACKAGE_RESTARTED.equals(intent.getAction())
+                || Intent.ACTION_PACKAGE_REPLACED.equals(intent.getAction())) {
+            isRunnerDummy = true;
+        }
+
+        if (isRunnerDummy) {
+            Subject.just(1)
+                    .subscribeOn(AndroidSchedulers.mainThread())
+                    .delay(new Random().nextInt(3000), TimeUnit.MILLISECONDS)
+                    .subscribe(i -> {
+                        Intent serviceIntent = new Intent(context, JandiDummyActivity.class);
+                        serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(serviceIntent);
+                    });
+        }
+    }
+}
