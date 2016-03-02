f67aa70d3acb49cfa9287ea438f0d6054b7c998a
Steve <steve@tosslab.com>
2016-03-02 15:42:51 +0900

    이미지 exif 정리

--- exif_clean.sh
+++ exif_clean.sh
@@ -1,2 +1,5 @@
-echo "find . -path '*src/main/res/*' -name '*.png' -exec exiftool -overwrite_original -all= {} \;"
+echo "Start Cleaning exif to Origins"
 find . -path '*src/main/res/*' -name '*.png' -exec exiftool -overwrite_original -all= {} \;
+echo "************************************"
+echo "Start Cleaning exif to Crawrer"
+find . -path '*src/main/res-crawrer/*' -name '*.png' -exec exiftool -overwrite_original -all= {} \;
