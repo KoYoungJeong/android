2cba53acf141f2e18bca73439858263b02f6f216
Kim Tae Nyun <newgto2001@gmail.com>
2016-03-11 18:32:16 +0900

    코드 리뷰 반영

--- app/src/main/java/com/tosslab/jandi/app/utils/StringCompareUtil.java
+++ app/src/main/java/com/tosslab/jandi/app/utils/StringCompareUtil.java
@@ -6,7 +6,7 @@ package com.tosslab.jandi.app.utils;
 public class StringCompareUtil {
 
     public static int compare(String string1, String string2) {
-        int end = string1.length() < string2.length() ? string2.length() : string2.length();
+        int end = Math.min(string1.length(), string2.length());
         char c1, c2;
         for (int i = 0; i < end; ++i) {
             if ((c1 = string1.charAt(i)) == (c2 = string2.charAt(i))) {
@@ -34,9 +34,21 @@ public class StringCompareUtil {
                 return -1;
             }
 
-            return c1 - c2;
+            // 같은 캐릭터라면 대문자가 먼저 나오도록
+            if (getAbsChar(c1) == getAbsChar(c2)) {
+                return c1 - c2;
+            }
+
+            return getAbsChar(c1) - getAbsChar(c2);
         }
         return 1;
+    }
+
+    private static int getAbsChar(char c) {
+        if (c >= 'a' && c <= 'z') {
+            return c - 32; // 대문자와 소문자의 아스키코드 차이
+        }
+        return c;
     }
 
     private static boolean isSpaceCharacter(char c) {
