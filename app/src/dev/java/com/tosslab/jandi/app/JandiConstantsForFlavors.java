fcfcec1091c79547ae9dc526fa7161b9855b8418
Steve <steve@tosslab.com>
2016-03-18 16:13:16 +0900

    [feature] fix url (JND-5204)

--- app/src/dev/java/com/tosslab/jandi/app/JandiConstantsForFlavors.java
+++ app/src/dev/java/com/tosslab/jandi/app/JandiConstantsForFlavors.java
@@ -11,14 +11,14 @@ public class JandiConstantsForFlavors {
     public static final String PARSE_APPLICATION_ID = "F61CAJEi4bXQ4S0me694noykr7ALCnFjEaR7yQcf";
     public static final String PARSE_CLIENT_KEY = "RP8MlBZfYVHRQiaZli0IEDzkpNSWulesgmSqmxke";
     // These constants are just for DEBUG mode
-    private static final String SERVICE_PROTOCOL = "http";
-    private static final String SERVICE_DOMAIN_BASE = "jandi.xyz";
+    private static final String SERVICE_PROTOCOL = "https";
+    private static final String SERVICE_DOMAIN_BASE = "jandi.io";
     private static final String SERVICE_DOMAIN_SUB = "i2";
     private static final String SERVICE_DOMAIN = SERVICE_DOMAIN_SUB + "." + SERVICE_DOMAIN_BASE;
     private static final String SERVICE_FILE_DOMAIN = "files" + "." + SERVICE_DOMAIN_BASE;
     private static final String SERVICE_BASE_DOMAIN = "www" + "." + SERVICE_DOMAIN_BASE;
     private static final String SERVICE_FILE_UPLOAD_DOMAIN = "upload" + "." + SERVICE_DOMAIN_BASE;
-    private static final String SERVICE_PORT = "80";
+    private static final String SERVICE_PORT = "443";
     public static final String SERVICE_ROOT_URL =
             SERVICE_PROTOCOL + "://" + SERVICE_DOMAIN + ":" + SERVICE_PORT + "/";
     public static final String SERVICE_INNER_API_URL = SERVICE_ROOT_URL + "inner-api";
@@ -29,9 +29,9 @@ public class JandiConstantsForFlavors {
     public static final String SERVICE_BASE_URL =
             SERVICE_PROTOCOL + "://" + SERVICE_BASE_DOMAIN + ":" + SERVICE_PORT + "/";
 
-    private static final String SOCKET_PROTOCOL = "http";
-    private static final String SOCKET_DOMAIN = "ws"+ "." + SERVICE_DOMAIN_BASE;
-    private static final String SOCKET_PORT = "80";
+    private static final String SOCKET_PROTOCOL = "https";
+    private static final String SOCKET_DOMAIN = "ws" + "." + SERVICE_DOMAIN_BASE;
+    private static final String SOCKET_PORT = "443";
     public static final String SOCKET_ROOT_URL =
             SOCKET_PROTOCOL + "://" + SOCKET_DOMAIN + ":" + SOCKET_PORT + "/";
 
