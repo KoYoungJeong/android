42f4ca06d6bb90b9e903df4c752be6f264f7122f
tonyjs <pkojun09@gmail.com>
2016-07-22 14:49:01 +0900

    [features] 메세지 검색 필터 화면 구현

---
+++ app/src/main/java/com/tosslab/jandi/app/ui/search/filter/member/MemberFilterActivity.java
@@ -0,0 +1,7 @@
+package com.tosslab.jandi.app.ui.search.filter.member;
+
+/**
+ * Created by tonyjs on 16. 7. 22..
+ */
+public class MemberFilterActivity {
+}
---
+++ app/src/main/java/com/tosslab/jandi/app/ui/search/filter/room/RoomFilterActivity.java
@@ -0,0 +1,7 @@
+package com.tosslab.jandi.app.ui.search.filter.room;
+
+/**
+ * Created by tonyjs on 16. 7. 22..
+ */
+public class RoomFilterActivity {
+}
