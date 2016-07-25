1a414e1a492cc2aa213bc69cae6c00cfd2191a39
tonyjs <pkojun09@gmail.com>
2016-07-25 11:34:13 +0900

    [features] 메세지 검색 필터 화면 구현 공통 툴바 백그라운드 추가

--- app/src/main/java/com/tosslab/jandi/app/ui/search/filter/member/MemberFilterActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/search/filter/member/MemberFilterActivity.java
@@ -1,7 +1,20 @@
 package com.tosslab.jandi.app.ui.search.filter.member;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.tosslab.jandi.app.R;
+import com.tosslab.jandi.app.ui.base.BaseAppCompatActivity;
+
 /**
  * Created by tonyjs on 16. 7. 22..
  */
-public class MemberFilterActivity {
+public class MemberFilterActivity extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_filter);
+    }
+
 }
