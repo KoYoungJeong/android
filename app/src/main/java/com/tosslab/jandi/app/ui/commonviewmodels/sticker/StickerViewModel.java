9b62efe7ed76ac1714780d320175edd6aa855e36
Tee <tee.kim@tosslab.com>
2016-08-24 16:36:54 +0900

    [Fix] 스티커 page 1개 일 때 무한 스크롤 없애기 (JA-536) - 스티커 페이지 한개일때 스크롤을 막음

--- app/src/main/java/com/tosslab/jandi/app/ui/commonviewmodels/sticker/StickerViewModel.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/commonviewmodels/sticker/StickerViewModel.java
@@ -126,6 +126,13 @@ public class StickerViewModel {
     private void addStickerView(List<ResMessages.StickerContent> stickers, ViewPager vgStickerItems) {
         int size = stickers.size();
 
+        //페이지가 한개만 있을 땐 스크롤을 막음
+        if (size <= 8) {
+            pagerStickerItems.setOnTouchListener((v, event) -> true);
+        } else {
+            pagerStickerItems.setOnTouchListener(null);
+        }
+
         if (size <= 0) {
             vgNoItemsLayout.setVisibility(View.VISIBLE);
             viewPagerIndicator.setVisibility(View.GONE);
@@ -156,6 +163,7 @@ public class StickerViewModel {
                     lastClickedTime = System.currentTimeMillis();
                 });
         vgStickerItems.setAdapter(adapter);
+
         vgStickerItems.setCurrentItem(adapter.getActualCount() * StickerViewPagerAdapter.PAGE_MULTIPLE / 2);
         viewPagerIndicator.setCurrentPosition(0);
         viewPagerIndicator.setIndicatorCount(adapter.getActualCount());
@@ -169,6 +177,7 @@ public class StickerViewModel {
                 }
             }
         });
+
     }
 
     public boolean isDoubleTap(long lastClickedTime) {
