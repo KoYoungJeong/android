c9884cbb552c28446f1fe127476ae74bab33a0e5
Steve Seong Jung <steve@tosslab.com>
2016-10-11 16:29:18 +0900

    [fix] 인트로 이미지 수정

--- app/src/main/java/com/tosslab/jandi/app/ui/intro/IntroActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/intro/IntroActivity.java
@@ -93,6 +93,7 @@ public class IntroActivity extends BaseAppCompatActivity implements IntroActivit
                     .subscribe(() -> {
                         splashDrawable.start();
                     });
+
         }
 
     }
