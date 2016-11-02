51b2506459c7a4e64246285477486f8db86d9e92
Steve Seong Jung <steve@tosslab.com>
2016-11-02 11:01:18 +0900

    [fix] 바이두 푸시 재적용

--- app/src/main/java/com/tosslab/jandi/app/utils/parse/PushUtil.java
+++ app/src/main/java/com/tosslab/jandi/app/utils/parse/PushUtil.java
@@ -1,14 +1,19 @@
 package com.tosslab.jandi.app.utils.parse;
 
+import com.baidu.android.pushservice.PushConstants;
+import com.baidu.android.pushservice.PushManager;
+import com.tosslab.jandi.app.JandiApplication;
+import com.tosslab.jandi.app.JandiConstantsForFlavors;
+
 /**
  * Created by Steve SeongUg Jung on 15. 6. 9..
  */
 public class PushUtil {
 
     public static void registPush() {
-//        PushManager.startWork(JandiApplication.getContext(),
-//                PushConstants.LOGIN_TYPE_API_KEY,
-//                JandiConstantsForFlavors.Push.BAIDU_API_KEY);
+        PushManager.startWork(JandiApplication.getContext(),
+                PushConstants.LOGIN_TYPE_API_KEY,
+                JandiConstantsForFlavors.Push.BAIDU_API_KEY);
     }
 
 }
