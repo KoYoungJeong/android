5037180a86bc000463154b559f2016f8eded0700
Steve SeongUg Jung <jsuch2362@gmail.com>
2016-12-07 18:13:47 +0900

    [fix] 모든 대화방 / 참여 대화방 정렬 순서 변경 (JA-814)

--- app/src/main/java/com/tosslab/jandi/app/ui/selector/room/RoomSelectorImpl.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/selector/room/RoomSelectorImpl.java
@@ -219,11 +219,11 @@ public class RoomSelectorImpl implements RoomSelector {
         if (isIncludeAllMember) {
 
             ExpandRoomData dummyData = new ExpandRoomData();
-            dummyData.setType(JandiConstants.Entity.TYPE_JOINED_ROOM);
+            dummyData.setType(JandiConstants.Entity.TYPE_EVERYWHERE);
             topicDatas.add(dummyData);
 
             dummyData = new ExpandRoomData();
-            dummyData.setType(JandiConstants.Entity.TYPE_EVERYWHERE);
+            dummyData.setType(JandiConstants.Entity.TYPE_JOINED_ROOM);
             topicDatas.add(dummyData);
         }
 
@@ -305,11 +305,11 @@ public class RoomSelectorImpl implements RoomSelector {
         if (isIncludeAllMember) {
 
             ExpandRoomData dummyData = new ExpandRoomData();
-            dummyData.setType(JandiConstants.Entity.TYPE_JOINED_ROOM);
+            dummyData.setType(JandiConstants.Entity.TYPE_EVERYWHERE);
             roomDatas.add(dummyData);
 
             dummyData = new ExpandRoomData();
-            dummyData.setType(JandiConstants.Entity.TYPE_EVERYWHERE);
+            dummyData.setType(JandiConstants.Entity.TYPE_JOINED_ROOM);
             roomDatas.add(dummyData);
         }
 
