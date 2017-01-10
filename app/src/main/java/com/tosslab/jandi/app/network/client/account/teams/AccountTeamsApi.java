c1445b396dacfa398110ffb6344089d87d489ca4
tee <newgto2001@gmail.com>
2017-01-10 17:23:21 +0900

    [Fix] 팀 탭의 초대 버튼 노출 개선 및 탈퇴기능 추가 외 1건 (JA-600 / JA-598) - 팀 탭의 초대 버튼 노출 개선 및 탈퇴기능 추가(JA-600) - 관리자 접속시 팀 탭에 관리자 메뉴 노출 (JA-598) - 해당 신규 기능 구현함.

---
+++ app/src/main/java/com/tosslab/jandi/app/network/client/account/teams/AccountTeamsApi.java
@@ -0,0 +1,37 @@
+package com.tosslab.jandi.app.network.client.account.teams;
+
+import com.tosslab.jandi.app.JandiConstants;
+import com.tosslab.jandi.app.network.client.ApiTemplate;
+import com.tosslab.jandi.app.network.exception.RetrofitException;
+import com.tosslab.jandi.app.network.manager.restapiclient.restadapterfactory.builder.RetrofitBuilder;
+import com.tosslab.jandi.app.network.models.ResCommon;
+
+import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.Headers;
+import retrofit2.http.Path;
+
+/**
+ * Created by tee on 2017. 1. 10..
+ */
+
+public class AccountTeamsApi extends ApiTemplate<AccountTeamsApi.Api> {
+
+    @Inject
+    public AccountTeamsApi(RetrofitBuilder retrofitBuilder) {
+        super(AccountTeamsApi.Api.class, retrofitBuilder);
+    }
+
+    public ResCommon requestLeaveTeam(long teamId) throws RetrofitException {
+        return call(() -> getApi().leaveTeam(teamId));
+    }
+
+    interface Api {
+        @DELETE("account/teams/{teamId}")
+        @Headers("Accept:" + JandiConstants.HTTP_ACCEPT_HEADER_DEFAULT)
+        Call<ResCommon> leaveTeam(@Path("teamId") long teamId);
+    }
+
+}
--- app/src/main/java/com/tosslab/jandi/app/ui/maintab/tabs/team/info/TeamInfoActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/maintab/tabs/team/info/TeamInfoActivity.java
@@ -5,26 +5,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.tosslab.jandi.app.Henson;
 import com.tosslab.jandi.app.R;
+import com.tosslab.jandi.app.local.orm.repositories.AccountRepository;
+import com.tosslab.jandi.app.local.orm.repositories.PollRepository;
+import com.tosslab.jandi.app.local.orm.repositories.info.InitialInfoRepository;
+import com.tosslab.jandi.app.network.client.account.teams.AccountTeamsApi;
+import com.tosslab.jandi.app.network.manager.restapiclient.restadapterfactory.builder.RetrofitBuilder;
 import com.tosslab.jandi.app.team.TeamInfoLoader;
 import com.tosslab.jandi.app.team.authority.Level;
 import com.tosslab.jandi.app.team.member.User;
 import com.tosslab.jandi.app.ui.base.BaseAppCompatActivity;
-import com.tosslab.jandi.app.ui.invites.InviteDialogExecutor;
 import com.tosslab.jandi.app.utils.ApplicationUtil;
-import com.tosslab.jandi.app.utils.analytics.AnalyticsUtil;
-import com.tosslab.jandi.app.utils.analytics.AnalyticsValue;
+import com.tosslab.jandi.app.utils.JandiPreference;
 import com.tosslab.jandi.app.views.profile.ProfileLabelView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import rx.Completable;
 import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 public class TeamInfoActivity extends BaseAppCompatActivity {
 
@@ -46,8 +54,8 @@ public class TeamInfoActivity extends BaseAppCompatActivity {
     @Bind(R.id.label_team_info_member_count)
     ProfileLabelView labelTeamMemberCount;
 
-    @Bind(R.id.btn_team_info_invite)
-    TextView BtnTeaminfoInvite;
+    @Bind(R.id.tv_team_leave)
+    TextView tvTeamLeave;
 
     public static void start(Context context) {
         Intent starter = new Intent(context, TeamInfoActivity.class);
@@ -73,7 +81,7 @@ public class TeamInfoActivity extends BaseAppCompatActivity {
 
     private void setUpAuthority() {
         if (TeamInfoLoader.getInstance().getMyLevel() == Level.Guest) {
-            BtnTeaminfoInvite.setVisibility(View.GONE);
+            tvTeamLeave.setVisibility(View.GONE);
         }
     }
 
@@ -85,10 +93,61 @@ public class TeamInfoActivity extends BaseAppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @OnClick(R.id.btn_team_info_invite)
-    void inviteMember() {
-        InviteDialogExecutor.getInstance().executeInvite(this);
-        AnalyticsUtil.sendEvent(AnalyticsValue.Screen.TeamInformation, AnalyticsValue.Action.InviteMember);
+    @OnClick(R.id.tv_team_leave)
+    void leaveTeam() {
+        // team leave 로직
+        if (TeamInfoLoader.getInstance().getMyLevel() == Level.Owner) {
+            showNoAllowedTeamLeaveDialog();
+        } else {
+            showTeamLeaveDialog();
+        }
+    }
+
+    private void showTeamLeaveDialog() {
+        new AlertDialog.Builder(this, R.style.JandiTheme_AlertDialog_FixWidth_300)
+                .setTitle(R.string.team_teamleave_member_title)
+                .setMessage(R.string.team_teamleave_member_desc)
+                .setPositiveButton(R.string.jandi_confirm, (dialog, which) -> {
+                    Completable.fromCallable(() -> {
+                        new AccountTeamsApi(RetrofitBuilder.getInstance()).requestLeaveTeam(TeamInfoLoader.getInstance().getTeamId());
+                        return Completable.complete();
+                    }).subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(() -> {
+                                leaveTeamAndGotoSelectTeam();
+                            }, t -> {
+                                t.printStackTrace();
+                            });
+                })
+                .setNegativeButton(R.string.jandi_cancel, null)
+                .create()
+                .show();
+    }
+
+    private void leaveTeamAndGotoSelectTeam() {
+        long teamId = TeamInfoLoader.getInstance().getTeamId();
+        AccountRepository.getRepository().removeSelectedTeamInfo();
+        startActivity(Henson.with(TeamInfoActivity.this)
+                .gotoTeamSelectListActivity()
+                .shouldRefreshAccountInfo(true)
+                .build());
+
+        InitialInfoRepository.getInstance().removeInitialInfo(teamId);
+        JandiPreference.setSocketConnectedLastTime(-1);
+
+        PollRepository.getInstance().clear(teamId);
+
+        TeamInfoLoader instance = TeamInfoLoader.getInstance();
+        instance = null;
+    }
+
+    private void showNoAllowedTeamLeaveDialog() {
+        new AlertDialog.Builder(this, R.style.JandiTheme_AlertDialog_FixWidth_300)
+                .setTitle(R.string.team_teamleave_owner_title)
+                .setMessage(R.string.team_teamleave_owner_desc)
+                .setPositiveButton(R.string.jandi_confirm, null)
+                .create()
+                .show();
     }
 
     private void setUpAppVersion() {
