e970039281e5cfed3babd2ec742a8fd4a7e03815
Seong-Ug Steve Jung <jsuch2362@gmail.com>
2017-02-07 17:56:16 +0900

    [fix] InitialInfo Json 처리 수정

--- app/src/main/java/com/tosslab/jandi/app/local/orm/repositories/info/InitialInfoRepository.java
+++ app/src/main/java/com/tosslab/jandi/app/local/orm/repositories/info/InitialInfoRepository.java
@@ -1,10 +1,18 @@
 package com.tosslab.jandi.app.local.orm.repositories.info;
 
-import com.j256.ormlite.dao.Dao;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.tosslab.jandi.app.JandiApplication;
 import com.tosslab.jandi.app.local.orm.repositories.template.LockExecutorTemplate;
 import com.tosslab.jandi.app.network.models.start.RawInitialInfo;
 
-import java.sql.SQLException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,82 +28,123 @@ public class InitialInfoRepository extends LockExecutorTemplate {
 
     public boolean upsertRawInitialInfo(RawInitialInfo info) {
         return execute(() -> {
+            File teamDir = getTeamDir();
+            File teamJson = new File(teamDir, getTeamJsonFileName(info.getTeamId()));
+            FileWriter fileWriter = null;
+            BufferedWriter bw = null;
             try {
-                Dao<RawInitialInfo, Object> dao = getDao(RawInitialInfo.class);
-                dao.createOrUpdate(info);
+                teamJson.createNewFile();
+                fileWriter = new FileWriter(teamJson);
+                bw = new BufferedWriter(fileWriter);
+                bw.write(info.getRawValue());
                 return true;
-            } catch (SQLException e) {
-                e.printStackTrace();
+            } catch (IOException e) {
                 return false;
+            } finally {
+                if (fileWriter != null) {
+                    try {
+                        fileWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (bw != null) {
+                    try {
+                        bw.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
 
+    private File getTeamDir() {
+        File rootDir = JandiApplication.getContext().getFilesDir();
+        File teamDir = new File(rootDir, "team_json");
+        if (!teamDir.exists()) {
+            teamDir.mkdirs();
+        }
+
+        return teamDir;
+    }
+
+    @NonNull
     public List<Long> getSavedTeamList() {
         return execute(() -> {
+
+            File teamDir = getTeamDir();
+            String[] list = teamDir.list();
+
             ArrayList<Long> teamIds = new ArrayList<>();
-            try {
-                Dao<RawInitialInfo, Object> dao = getDao(RawInitialInfo.class);
-                List<RawInitialInfo> teams = dao.queryBuilder().selectColumns("teamId").query();
-                for (RawInitialInfo team : teams) {
-                    teamIds.add(team.getTeamId());
+            for (String teamJson : list) {
+                int idx = teamJson.indexOf(".");
+                if (idx > 0) {
+                    String teamId = teamJson.substring(0, idx);
+                    try {
+                        teamIds.add(Long.parseLong(teamId));
+                    } catch (NumberFormatException e) {
+                        e.printStackTrace();
+                    }
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
             return teamIds;
         });
     }
 
+    @Nullable
     public RawInitialInfo getRawInitialInfo(long teamId) {
         return execute(() -> {
-            try {
-                Dao<RawInitialInfo, Long> dao = getDao(RawInitialInfo.class);
-                return dao.queryForId(teamId);
-            } catch (SQLException e) {
+
+            File teamDir = getTeamDir();
+            File teamJson = new File(teamDir, getTeamJsonFileName(teamId));
+            if (teamJson.exists()) {
+                try {
+                    FileReader fileReader = new FileReader(teamJson);
+                    BufferedReader br = new BufferedReader(fileReader);
+                    String temp = null;
+                    StringBuilder sb = new StringBuilder();
+                    while ((temp = br.readLine()) != null) {
+                        sb.append(temp);
+                    }
+                    return new RawInitialInfo(teamId, sb.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            } else {
                 return null;
             }
         });
     }
 
+    @NonNull
+    private String getTeamJsonFileName(long teamId) {return teamId + ".json";}
+
     public boolean hasInitialInfo(long teamId) {
         return execute(() -> {
-            try {
-                Dao<RawInitialInfo, Long> dao = getDao(RawInitialInfo.class);
-                return dao.queryBuilder()
-                        .where()
-                        .eq("teamId", teamId)
-                        .countOf() > 0;
-            } catch (SQLException e) {
-                return false;
-            }
+            File teamDir = getTeamDir();
+            File file = new File(teamDir, getTeamJsonFileName(teamId));
+            return file.exists();
         });
     }
 
     public boolean removeInitialInfo(long teamId) {
         return execute(() -> {
 
-            try {
-                Dao<RawInitialInfo, Long> dao = getDao(RawInitialInfo.class);
-                return dao.deleteById(teamId) > 0;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return false;
-            }
+            File teamDir = getTeamDir();
+            File file = new File(teamDir, getTeamJsonFileName(teamId));
+            return file.delete();
         });
     }
 
     public boolean clear() {
         return execute(() -> {
-            try {
-                Dao<RawInitialInfo, Long> dao = getDao(RawInitialInfo.class);
-                return dao.deleteBuilder()
-                        .delete() > 0;
-            } catch (SQLException e) {
-                e.printStackTrace();
-                return false;
+            File[] files = getTeamDir().listFiles();
+            for (File file : files) {
+                file.delete();
             }
+            return true;
         });
     }
-
 }
