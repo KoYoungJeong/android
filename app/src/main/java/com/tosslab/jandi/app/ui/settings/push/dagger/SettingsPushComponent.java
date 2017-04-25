1bcf95f42e8834a4b14b47b66005fe362061e8af
tee <newgto2001@gmail.com>
2017-04-25 10:20:45 +0900

    소스 정리

--- app/src/main/java/com/tosslab/jandi/app/ui/settings/push/dagger/SettingsPushComponent.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/settings/push/dagger/SettingsPushComponent.java
@@ -2,11 +2,10 @@ package com.tosslab.jandi.app.ui.settings.push.dagger;
 
 import com.tosslab.jandi.app.network.dagger.ApiClientModule;
 import com.tosslab.jandi.app.ui.settings.push.SettingsPushFragment;
-import com.tosslab.jandi.app.ui.settings.push.schedule.dagger.SettingPushScheduleModule;
 
 import dagger.Component;
 
-@Component(modules = {ApiClientModule.class, SettingsPushModule.class, SettingPushScheduleModule.class})
+@Component(modules = {ApiClientModule.class, SettingsPushModule.class})
 public interface SettingsPushComponent {
     void inject(SettingsPushFragment fragment);
 }
