2fb85a5febb3b1b171ea64d3cb62fd90383df13b
Tee <tee.kim@tosslab.com>
2017-05-15 15:17:15 +0900

    핫 픽스 멤버 프로필 가로모드 수정

--- app/src/main/java/com/tosslab/jandi/app/ui/profile/member/MemberProfileActivity.java
+++ app/src/main/java/com/tosslab/jandi/app/ui/profile/member/MemberProfileActivity.java
@@ -140,6 +140,7 @@ public class MemberProfileActivity extends BaseAppCompatActivity {
     @Bind(R.id.v_button_divider)
     View vButtonDivider;
     @Bind(R.id.v_start_bottom_line)
+    @Nullable
     View vStartBottomLine;
 
     ProfileLoader profileLoader;
@@ -204,14 +205,17 @@ public class MemberProfileActivity extends BaseAppCompatActivity {
         if (!isBot) {
             if (!TeamInfoLoader.getInstance().getUser(memberId).isInactive()) {
                 profileLoader = new MemberProfileLoader();
-                vStartBottomLine.setVisibility(View.VISIBLE);
+                if (vStartBottomLine != null)
+                    vStartBottomLine.setVisibility(View.VISIBLE);
             } else {
                 profileLoader = new InactivedMemberProfileLoader();
-                vStartBottomLine.setVisibility(View.INVISIBLE);
+                if (vStartBottomLine != null)
+                    vStartBottomLine.setVisibility(View.INVISIBLE);
             }
         } else {
             profileLoader = new JandiBotProfileLoader();
-            vStartBottomLine.setVisibility(View.INVISIBLE);
+            if (vStartBottomLine != null)
+                vStartBottomLine.setVisibility(View.INVISIBLE);
         }
     }
 
