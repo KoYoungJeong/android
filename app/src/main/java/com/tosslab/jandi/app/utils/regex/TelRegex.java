3914f20de332a4f04f8937464df8e66a685d5be5
Tee <tee.kim@tosslab.com>
2017-05-23 18:10:27 +0900

    [Fix] 메세지 내 전화번호 형식 개선건 (JA-1227) - 국번 4자리까지 지원하도록 수정

--- app/src/main/java/com/tosslab/jandi/app/utils/regex/TelRegex.java
+++ app/src/main/java/com/tosslab/jandi/app/utils/regex/TelRegex.java
@@ -6,17 +6,17 @@ import java.util.regex.Pattern;
  * Created by tonyjs on 15. 8. 31..
  */
 public class TelRegex {
+
     // (021234567, 02 123 4567, 02.123.4567, 02-123-4567)
     private static final String PHONE_NUMBER_WITHOUT_SPACE = "(\\d{9,11})";
-    private static final String PHONE_NUMBER_WITH_SPACE = "(\\d{2,3}\\s\\d{3,4}\\s\\d{4})";
-    private static final String PHONE_NUMBER_WITH_DOT = "(\\d{2,3}\\.\\d{3,4}\\.\\d{4})";
-    private static final String PHONE_NUMBER_WITH_DASH = "(\\d{2,3}\\-\\d{3,4}\\-\\d{4})";
+    private static final String PHONE_NUMBER_WITH_SPACE = "(\\d{2,4}\\s\\d{3,4}\\s\\d{4})";
+    private static final String PHONE_NUMBER_WITH_DOT = "(\\d{2,4}\\.\\d{3,4}\\.\\d{4})";
+    private static final String PHONE_NUMBER_WITH_DASH = "(\\d{2,4}\\-\\d{3,4}\\-\\d{4})";
     // 국가번호는 1000의 자리까지 존재하고 국가번호가 들어가면 지역번호 앞자리가 생략된다.(+10 ~ +999999)
     private static final String GLOBAL_PHONE_NUMBER_WITHOUT_SPACE = "(\\+\\d{9,14})";
     private static final String GLOBAL_PHONE_NUMBER_WITH_SPACE = "(\\+\\d{2,6}\\s\\d{3,4}\\s\\d{4})|(\\+\\d{1,4}\\s\\d{1,2}\\s\\d{3,4}\\s\\d{4})";
     private static final String GLOBAL_PHONE_NUMBER_WITH_DOT = "(\\+\\d{2,6}\\.\\d{3,4}\\.\\d{4})|(\\+\\d{1,4}\\s\\d{1,2}\\.\\d{3,4}\\.\\d{4})";
     private static final String GLOBAL_PHONE_NUMBER_WITH_DASH = "(\\+\\d{2,6}\\-\\d{3,4}\\-\\d{4})|(\\+\\d{1,4}\\s\\d{1,2}\\-\\d{3,4}\\-\\d{4})";
-
     public static final String VALID_TEL_PATTERN_STRING = PHONE_NUMBER_WITHOUT_SPACE + "|"
             + PHONE_NUMBER_WITH_SPACE + "|"
             + PHONE_NUMBER_WITH_DOT + "|"
@@ -25,7 +25,7 @@ public class TelRegex {
             + GLOBAL_PHONE_NUMBER_WITH_SPACE + "|"
             + GLOBAL_PHONE_NUMBER_WITH_DOT + "|"
             + GLOBAL_PHONE_NUMBER_WITH_DASH;
-
     public static final Pattern VALID_TEL_PATTERN = Pattern.compile(
             VALID_TEL_PATTERN_STRING, Pattern.CASE_INSENSITIVE);
+
 }
